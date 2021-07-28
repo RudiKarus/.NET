677f1d65f8eae3e3898f08d0d7cee3605bd667de
R-U-D-I <88041763+R-U-D-I@users.noreply.github.com>
2021-07-28 11:26:53 +0200

    Add files via upload

---
+++ ForSchleife/ForSchleife/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace ForSchleife
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CmdSchleife1 = new System.Windows.Forms.Button();
+            this.LblAnzeige1 = new System.Windows.Forms.Label();
+            this.LblAnzeige2 = new System.Windows.Forms.Label();
+            this.CmdSchleife2 = new System.Windows.Forms.Button();
+            this.LblAnzeige3 = new System.Windows.Forms.Label();
+            this.CmdSchleife3 = new System.Windows.Forms.Button();
+            this.LblAnzeige4 = new System.Windows.Forms.Label();
+            this.CmdSchleife4 = new System.Windows.Forms.Button();
+            this.LblAnzeige5 = new System.Windows.Forms.Label();
+            this.CmdSchleife5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CmdSchleife1
+            // 
+            this.CmdSchleife1.Location = new System.Drawing.Point(12, 24);
+            this.CmdSchleife1.Name = "CmdSchleife1";
+            this.CmdSchleife1.Size = new System.Drawing.Size(125, 35);
+            this.CmdSchleife1.TabIndex = 0;
+            this.CmdSchleife1.Text = "Schleife 1";
+            this.CmdSchleife1.UseVisualStyleBackColor = true;
+            this.CmdSchleife1.Click += new System.EventHandler(this.CmdSchleife1_Click);
+            // 
+            // LblAnzeige1
+            // 
+            this.LblAnzeige1.AutoSize = true;
+            this.LblAnzeige1.Location = new System.Drawing.Point(57, 71);
+            this.LblAnzeige1.Name = "LblAnzeige1";
+            this.LblAnzeige1.Size = new System.Drawing.Size(0, 25);
+            this.LblAnzeige1.TabIndex = 1;
+            // 
+            // LblAnzeige2
+            // 
+            this.LblAnzeige2.AutoSize = true;
+            this.LblAnzeige2.Location = new System.Drawing.Point(211, 71);
+            this.LblAnzeige2.Name = "LblAnzeige2";
+            this.LblAnzeige2.Size = new System.Drawing.Size(0, 25);
+            this.LblAnzeige2.TabIndex = 3;
+            // 
+            // CmdSchleife2
+            // 
+            this.CmdSchleife2.Location = new System.Drawing.Point(166, 24);
+            this.CmdSchleife2.Name = "CmdSchleife2";
+            this.CmdSchleife2.Size = new System.Drawing.Size(125, 35);
+            this.CmdSchleife2.TabIndex = 2;
+            this.CmdSchleife2.Text = "Schleife 2";
+            this.CmdSchleife2.UseVisualStyleBackColor = true;
+            this.CmdSchleife2.Click += new System.EventHandler(this.CmdSchleife2_Click);
+            // 
+            // LblAnzeige3
+            // 
+            this.LblAnzeige3.AutoSize = true;
+            this.LblAnzeige3.Location = new System.Drawing.Point(374, 71);
+            this.LblAnzeige3.Name = "LblAnzeige3";
+            this.LblAnzeige3.Size = new System.Drawing.Size(0, 25);
+            this.LblAnzeige3.TabIndex = 5;
+            // 
+            // CmdSchleife3
+            // 
+            this.CmdSchleife3.Location = new System.Drawing.Point(329, 24);
+            this.CmdSchleife3.Name = "CmdSchleife3";
+            this.CmdSchleife3.Size = new System.Drawing.Size(125, 35);
+            this.CmdSchleife3.TabIndex = 4;
+            this.CmdSchleife3.Text = "Schleife 3";
+            this.CmdSchleife3.UseVisualStyleBackColor = true;
+            this.CmdSchleife3.Click += new System.EventHandler(this.CmdSchleife3_Click);
+            // 
+            // LblAnzeige4
+            // 
+            this.LblAnzeige4.AutoSize = true;
+            this.LblAnzeige4.Location = new System.Drawing.Point(538, 71);
+            this.LblAnzeige4.Name = "LblAnzeige4";
+            this.LblAnzeige4.Size = new System.Drawing.Size(0, 25);
+            this.LblAnzeige4.TabIndex = 7;
+            // 
+            // CmdSchleife4
+            // 
+            this.CmdSchleife4.Location = new System.Drawing.Point(493, 24);
+            this.CmdSchleife4.Name = "CmdSchleife4";
+            this.CmdSchleife4.Size = new System.Drawing.Size(125, 35);
+            this.CmdSchleife4.TabIndex = 6;
+            this.CmdSchleife4.Text = "Schleife 4";
+            this.CmdSchleife4.UseVisualStyleBackColor = true;
+            this.CmdSchleife4.Click += new System.EventHandler(this.CmdSchleife4_Click);
+            // 
+            // LblAnzeige5
+            // 
+            this.LblAnzeige5.AutoSize = true;
+            this.LblAnzeige5.Location = new System.Drawing.Point(701, 71);
+            this.LblAnzeige5.Name = "LblAnzeige5";
+            this.LblAnzeige5.Size = new System.Drawing.Size(0, 25);
+            this.LblAnzeige5.TabIndex = 9;
+            // 
+            // CmdSchleife5
+            // 
+            this.CmdSchleife5.Location = new System.Drawing.Point(656, 24);
+            this.CmdSchleife5.Name = "CmdSchleife5";
+            this.CmdSchleife5.Size = new System.Drawing.Size(125, 35);
+            this.CmdSchleife5.TabIndex = 8;
+            this.CmdSchleife5.Text = "Schleife 5";
+            this.CmdSchleife5.UseVisualStyleBackColor = true;
+            this.CmdSchleife5.Click += new System.EventHandler(this.CmdSchleife5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(800, 319);
+            this.Controls.Add(this.LblAnzeige5);
+            this.Controls.Add(this.CmdSchleife5);
+            this.Controls.Add(this.LblAnzeige4);
+            this.Controls.Add(this.CmdSchleife4);
+            this.Controls.Add(this.LblAnzeige3);
+            this.Controls.Add(this.CmdSchleife3);
+            this.Controls.Add(this.LblAnzeige2);
+            this.Controls.Add(this.CmdSchleife2);
+            this.Controls.Add(this.LblAnzeige1);
+            this.Controls.Add(this.CmdSchleife1);
+            this.Name = "Form1";
+            this.Text = "For-Schleife";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CmdSchleife1;
+        private System.Windows.Forms.Label LblAnzeige1;
+        private System.Windows.Forms.Label LblAnzeige2;
+        private System.Windows.Forms.Button CmdSchleife2;
+        private System.Windows.Forms.Label LblAnzeige3;
+        private System.Windows.Forms.Button CmdSchleife3;
+        private System.Windows.Forms.Label LblAnzeige4;
+        private System.Windows.Forms.Button CmdSchleife4;
+        private System.Windows.Forms.Label LblAnzeige5;
+        private System.Windows.Forms.Button CmdSchleife5;
+    }
+}
+
---
+++ ForSchleife/ForSchleife/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ForSchleife
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CmdSchleife1_Click(object sender, EventArgs e)
+        {
+            int i;
+            LblAnzeige1.Text = "";
+
+            for(i=3; i <= 7; i++)
+            {
+                LblAnzeige1.Text += i + "\n";
+            }
+        }
+
+        private void CmdSchleife2_Click(object sender, EventArgs e)
+        {
+            LblAnzeige2.Text = "";
+
+            for(int i=3; i<=11; i = i+2)
+            {
+                LblAnzeige2.Text += i + "\n";
+            }
+        }
+
+        private void CmdSchleife3_Click(object sender, EventArgs e)
+        {
+            LblAnzeige3.Text = "";
+
+            for(int i=7; i>=3; i--)
+            {
+                LblAnzeige3.Text += i + "\n";
+            }
+        }
+
+        private void CmdSchleife4_Click(object sender, EventArgs e)
+        {
+            LblAnzeige4.Text = "";
+
+            for(double d=1.5; d<=7.5; d=d+1.5)
+            {
+                LblAnzeige4.Text += d + "\n";
+            }
+
+        }
+
+        private void CmdSchleife5_Click(object sender, EventArgs e)
+        {
+            LblAnzeige5.Text = "";
+
+            for(int i=3; i<=20; i++)
+            {
+                if (i >= 5 && i <= 7)
+                    continue;
+                if (i >= 11)
+                    break;
+                LblAnzeige5.Text += i + "\n";
+            }
+        }
+    }
+}
