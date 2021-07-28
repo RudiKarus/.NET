597070b1e18b8526fe1f2c1704cbf5848e1c4ebd
R-U-D-I <88041763+R-U-D-I@users.noreply.github.com>
2021-07-28 11:46:47 +0200

    Add files via upload

---
+++ Mathematik/Mathematik/Form1.Designer.cs
@@ -0,0 +1,423 @@
+﻿namespace Mathematik
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.T = new System.Windows.Forms.TextBox();
+            this.CmdBackSpace = new System.Windows.Forms.Button();
+            this.CmdClear = new System.Windows.Forms.Button();
+            this.CmdSinus = new System.Windows.Forms.Button();
+            this.CmdCosinus = new System.Windows.Forms.Button();
+            this.CmdTangens = new System.Windows.Forms.Button();
+            this.CmdLn = new System.Windows.Forms.Button();
+            this.CmdLog = new System.Windows.Forms.Button();
+            this.CmdE = new System.Windows.Forms.Button();
+            this.CmdCeiling = new System.Windows.Forms.Button();
+            this.CmdFloor = new System.Windows.Forms.Button();
+            this.CmdRound = new System.Windows.Forms.Button();
+            this.CmdTruncate = new System.Windows.Forms.Button();
+            this.CmdWurzel = new System.Windows.Forms.Button();
+            this.CmdPlusMinus = new System.Windows.Forms.Button();
+            this.CmdKehrwert = new System.Windows.Forms.Button();
+            this.CmdZiffer = new System.Windows.Forms.Button();
+            this.CmdKomma = new System.Windows.Forms.Button();
+            this.CmdPi = new System.Windows.Forms.Button();
+            this.ChkInv = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // T
+            // 
+            this.T.Location = new System.Drawing.Point(27, 45);
+            this.T.Name = "T";
+            this.T.Size = new System.Drawing.Size(355, 31);
+            this.T.TabIndex = 0;
+            this.T.TextChanged += new System.EventHandler(this.T_TextChanged);
+            // 
+            // CmdBackSpace
+            // 
+            this.CmdBackSpace.Location = new System.Drawing.Point(166, 99);
+            this.CmdBackSpace.Name = "CmdBackSpace";
+            this.CmdBackSpace.Size = new System.Drawing.Size(50, 23);
+            this.CmdBackSpace.TabIndex = 1;
+            this.CmdBackSpace.Text = "BS";
+            this.CmdBackSpace.UseVisualStyleBackColor = true;
+            this.CmdBackSpace.Click += new System.EventHandler(this.CmdBackSpace_Click);
+            // 
+            // CmdClear
+            // 
+            this.CmdClear.Location = new System.Drawing.Point(222, 99);
+            this.CmdClear.Name = "CmdClear";
+            this.CmdClear.Size = new System.Drawing.Size(50, 23);
+            this.CmdClear.TabIndex = 2;
+            this.CmdClear.Text = "Clear";
+            this.CmdClear.UseVisualStyleBackColor = true;
+            this.CmdClear.Click += new System.EventHandler(this.CmdClear_Click);
+            // 
+            // CmdSinus
+            // 
+            this.CmdSinus.Location = new System.Drawing.Point(27, 96);
+            this.CmdSinus.Name = "CmdSinus";
+            this.CmdSinus.Size = new System.Drawing.Size(50, 23);
+            this.CmdSinus.TabIndex = 3;
+            this.CmdSinus.Text = "sin";
+            this.CmdSinus.UseVisualStyleBackColor = true;
+            this.CmdSinus.Click += new System.EventHandler(this.CmdSinus_Click);
+            // 
+            // CmdCosinus
+            // 
+            this.CmdCosinus.Location = new System.Drawing.Point(27, 125);
+            this.CmdCosinus.Name = "CmdCosinus";
+            this.CmdCosinus.Size = new System.Drawing.Size(50, 23);
+            this.CmdCosinus.TabIndex = 4;
+            this.CmdCosinus.Text = "cos";
+            this.CmdCosinus.UseVisualStyleBackColor = true;
+            this.CmdCosinus.Click += new System.EventHandler(this.CmdCosinus_Click);
+            // 
+            // CmdTangens
+            // 
+            this.CmdTangens.Location = new System.Drawing.Point(27, 154);
+            this.CmdTangens.Name = "CmdTangens";
+            this.CmdTangens.Size = new System.Drawing.Size(50, 23);
+            this.CmdTangens.TabIndex = 5;
+            this.CmdTangens.Text = "tan";
+            this.CmdTangens.UseVisualStyleBackColor = true;
+            this.CmdTangens.Click += new System.EventHandler(this.CmdTangens_Click);
+            // 
+            // CmdLn
+            // 
+            this.CmdLn.Location = new System.Drawing.Point(27, 212);
+            this.CmdLn.Name = "CmdLn";
+            this.CmdLn.Size = new System.Drawing.Size(50, 23);
+            this.CmdLn.TabIndex = 6;
+            this.CmdLn.Text = "ln";
+            this.CmdLn.UseVisualStyleBackColor = true;
+            this.CmdLn.Click += new System.EventHandler(this.CmdLn_Click);
+            // 
+            // CmdLog
+            // 
+            this.CmdLog.Location = new System.Drawing.Point(27, 241);
+            this.CmdLog.Name = "CmdLog";
+            this.CmdLog.Size = new System.Drawing.Size(50, 23);
+            this.CmdLog.TabIndex = 7;
+            this.CmdLog.Text = "log";
+            this.CmdLog.UseVisualStyleBackColor = true;
+            this.CmdLog.Click += new System.EventHandler(this.CmdLog_Click);
+            // 
+            // CmdE
+            // 
+            this.CmdE.Location = new System.Drawing.Point(250, 291);
+            this.CmdE.Name = "CmdE";
+            this.CmdE.Size = new System.Drawing.Size(50, 23);
+            this.CmdE.TabIndex = 8;
+            this.CmdE.Text = "E";
+            this.CmdE.UseVisualStyleBackColor = true;
+            this.CmdE.Click += new System.EventHandler(this.CmdE_Click);
+            // 
+            // CmdCeiling
+            // 
+            this.CmdCeiling.Location = new System.Drawing.Point(307, 143);
+            this.CmdCeiling.Name = "CmdCeiling";
+            this.CmdCeiling.Size = new System.Drawing.Size(75, 23);
+            this.CmdCeiling.TabIndex = 9;
+            this.CmdCeiling.Text = "Ceiling";
+            this.CmdCeiling.UseVisualStyleBackColor = true;
+            this.CmdCeiling.Click += new System.EventHandler(this.CmdCeiling_Click);
+            // 
+            // CmdFloor
+            // 
+            this.CmdFloor.Location = new System.Drawing.Point(307, 172);
+            this.CmdFloor.Name = "CmdFloor";
+            this.CmdFloor.Size = new System.Drawing.Size(75, 23);
+            this.CmdFloor.TabIndex = 10;
+            this.CmdFloor.Text = "Floor";
+            this.CmdFloor.UseVisualStyleBackColor = true;
+            this.CmdFloor.Click += new System.EventHandler(this.CmdFloor_Click);
+            // 
+            // CmdRound
+            // 
+            this.CmdRound.Location = new System.Drawing.Point(307, 201);
+            this.CmdRound.Name = "CmdRound";
+            this.CmdRound.Size = new System.Drawing.Size(75, 23);
+            this.CmdRound.TabIndex = 11;
+            this.CmdRound.Text = "Round";
+            this.CmdRound.UseVisualStyleBackColor = true;
+            this.CmdRound.Click += new System.EventHandler(this.CmdRound_Click);
+            // 
+            // CmdTruncate
+            // 
+            this.CmdTruncate.Location = new System.Drawing.Point(307, 230);
+            this.CmdTruncate.Name = "CmdTruncate";
+            this.CmdTruncate.Size = new System.Drawing.Size(75, 23);
+            this.CmdTruncate.TabIndex = 12;
+            this.CmdTruncate.Text = "Truncate";
+            this.CmdTruncate.UseVisualStyleBackColor = true;
+            this.CmdTruncate.Click += new System.EventHandler(this.CmdTruncate_Click);
+            // 
+            // CmdWurzel
+            // 
+            this.CmdWurzel.Location = new System.Drawing.Point(27, 183);
+            this.CmdWurzel.Name = "CmdWurzel";
+            this.CmdWurzel.Size = new System.Drawing.Size(50, 23);
+            this.CmdWurzel.TabIndex = 13;
+            this.CmdWurzel.Text = "Sqrt";
+            this.CmdWurzel.UseVisualStyleBackColor = true;
+            this.CmdWurzel.Click += new System.EventHandler(this.CmdWurzel_Click);
+            // 
+            // CmdPlusMinus
+            // 
+            this.CmdPlusMinus.Location = new System.Drawing.Point(82, 291);
+            this.CmdPlusMinus.Name = "CmdPlusMinus";
+            this.CmdPlusMinus.Size = new System.Drawing.Size(50, 23);
+            this.CmdPlusMinus.TabIndex = 14;
+            this.CmdPlusMinus.Text = "+/-";
+            this.CmdPlusMinus.UseVisualStyleBackColor = true;
+            this.CmdPlusMinus.Click += new System.EventHandler(this.CmdPlusMinus_Click);
+            // 
+            // CmdKehrwert
+            // 
+            this.CmdKehrwert.Location = new System.Drawing.Point(138, 291);
+            this.CmdKehrwert.Name = "CmdKehrwert";
+            this.CmdKehrwert.Size = new System.Drawing.Size(50, 23);
+            this.CmdKehrwert.TabIndex = 15;
+            this.CmdKehrwert.Text = "1/x";
+            this.CmdKehrwert.UseVisualStyleBackColor = true;
+            this.CmdKehrwert.Click += new System.EventHandler(this.CmdKehrwert_Click);
+            // 
+            // CmdZiffer
+            // 
+            this.CmdZiffer.Location = new System.Drawing.Point(166, 230);
+            this.CmdZiffer.Name = "CmdZiffer";
+            this.CmdZiffer.Size = new System.Drawing.Size(50, 23);
+            this.CmdZiffer.TabIndex = 16;
+            this.CmdZiffer.Text = "0";
+            this.CmdZiffer.UseVisualStyleBackColor = true;
+            this.CmdZiffer.Click += new System.EventHandler(this.CmdZiffer_Click);
+            // 
+            // CmdKomma
+            // 
+            this.CmdKomma.Location = new System.Drawing.Point(222, 230);
+            this.CmdKomma.Name = "CmdKomma";
+            this.CmdKomma.Size = new System.Drawing.Size(50, 23);
+            this.CmdKomma.TabIndex = 17;
+            this.CmdKomma.Text = ",";
+            this.CmdKomma.UseVisualStyleBackColor = true;
+            this.CmdKomma.Click += new System.EventHandler(this.CmdKomma_Click);
+            // 
+            // CmdPi
+            // 
+            this.CmdPi.Location = new System.Drawing.Point(194, 291);
+            this.CmdPi.Name = "CmdPi";
+            this.CmdPi.Size = new System.Drawing.Size(50, 23);
+            this.CmdPi.TabIndex = 18;
+            this.CmdPi.Text = "Pi";
+            this.CmdPi.UseVisualStyleBackColor = true;
+            this.CmdPi.Click += new System.EventHandler(this.CmdPi_Click);
+            // 
+            // ChkInv
+            // 
+            this.ChkInv.AutoSize = true;
+            this.ChkInv.Location = new System.Drawing.Point(307, 97);
+            this.ChkInv.Name = "ChkInv";
+            this.ChkInv.Size = new System.Drawing.Size(72, 29);
+            this.ChkInv.TabIndex = 19;
+            this.ChkInv.Text = "Inv";
+            this.ChkInv.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(110, 201);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 23);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.CmdZiffer_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(166, 201);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 23);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.CmdZiffer_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(222, 201);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 23);
+            this.button3.TabIndex = 22;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.CmdZiffer_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(222, 172);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(50, 23);
+            this.button4.TabIndex = 25;
+            this.button4.Text = "6";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.CmdZiffer_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(166, 172);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(50, 23);
+            this.button5.TabIndex = 24;
+            this.button5.Text = "5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.CmdZiffer_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(110, 172);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(50, 23);
+            this.button6.TabIndex = 23;
+            this.button6.Text = "4";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.CmdZiffer_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(222, 143);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(50, 23);
+            this.button7.TabIndex = 28;
+            this.button7.Text = "9";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.CmdZiffer_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(166, 143);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(50, 23);
+            this.button8.TabIndex = 27;
+            this.button8.Text = "8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.CmdZiffer_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(110, 143);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(50, 23);
+            this.button9.TabIndex = 26;
+            this.button9.Text = "7";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.CmdZiffer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(416, 352);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ChkInv);
+            this.Controls.Add(this.CmdPi);
+            this.Controls.Add(this.CmdKomma);
+            this.Controls.Add(this.CmdZiffer);
+            this.Controls.Add(this.CmdKehrwert);
+            this.Controls.Add(this.CmdPlusMinus);
+            this.Controls.Add(this.CmdWurzel);
+            this.Controls.Add(this.CmdTruncate);
+            this.Controls.Add(this.CmdRound);
+            this.Controls.Add(this.CmdFloor);
+            this.Controls.Add(this.CmdCeiling);
+            this.Controls.Add(this.CmdE);
+            this.Controls.Add(this.CmdLog);
+            this.Controls.Add(this.CmdLn);
+            this.Controls.Add(this.CmdTangens);
+            this.Controls.Add(this.CmdCosinus);
+            this.Controls.Add(this.CmdSinus);
+            this.Controls.Add(this.CmdClear);
+            this.Controls.Add(this.CmdBackSpace);
+            this.Controls.Add(this.T);
+            this.Name = "Form1";
+            this.Text = "Mini Taschenrechner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox T;
+        private System.Windows.Forms.Button CmdBackSpace;
+        private System.Windows.Forms.Button CmdClear;
+        private System.Windows.Forms.Button CmdSinus;
+        private System.Windows.Forms.Button CmdCosinus;
+        private System.Windows.Forms.Button CmdTangens;
+        private System.Windows.Forms.Button CmdLn;
+        private System.Windows.Forms.Button CmdLog;
+        private System.Windows.Forms.Button CmdE;
+        private System.Windows.Forms.Button CmdCeiling;
+        private System.Windows.Forms.Button CmdFloor;
+        private System.Windows.Forms.Button CmdRound;
+        private System.Windows.Forms.Button CmdTruncate;
+        private System.Windows.Forms.Button CmdWurzel;
+        private System.Windows.Forms.Button CmdPlusMinus;
+        private System.Windows.Forms.Button CmdKehrwert;
+        private System.Windows.Forms.Button CmdZiffer;
+        private System.Windows.Forms.Button CmdKomma;
+        private System.Windows.Forms.Button CmdPi;
+        private System.Windows.Forms.CheckBox ChkInv;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+    }
+}
+
---
+++ Mathematik/Mathematik/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mathematik
+{
+    public partial class Form1 : Form
+    {
+
+        private double x;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void T_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                x = Convert.ToDouble(T.Text);
+            }
+            catch
+            {
+                T.Text= "";
+                x = 0;
+            }
+        }
+
+
+        private void CmdBackSpace_Click(object sender, EventArgs e)
+        {
+            T.Text = T.Text.Substring(0, T.Text.Length - 1);
+        }
+
+
+        private void CmdClear_Click(object sender, EventArgs e)
+        {
+            T.Text = "";
+        }
+
+
+        private void CmdSinus_Click(object sender, EventArgs e)
+        {
+            if(ChkInv.Checked)
+            {
+                T.Text = "" + Math.Asin(x) * 180 / Math.PI;
+                ChkInv.Checked = false;
+            }
+            else
+                T.Text = "" + Math.Sin (x/ 180.0 * Math.PI);
+        }
+
+
+        private void CmdCosinus_Click(object sender, EventArgs e)
+        {
+            if (ChkInv.Checked)
+            {
+                T.Text = "" + Math.Acos(x) * 180 / Math.PI;
+            }
+            else
+                T.Text = "" + Math.Cos(x / 180.0 * Math.PI);
+        }
+
+
+        private void CmdTangens_Click(object sender, EventArgs e)
+        {
+            if (ChkInv.Checked)
+            {
+                T.Text = "" + Math.Atan(x) * 180 / Math.PI;
+                ChkInv.Checked = false;
+            }
+            else
+                T.Text = "" + Math.Tan(x / 180.0 * Math.PI);
+        }
+
+
+        private void CmdLn_Click(object sender, EventArgs e)
+        {
+            if (ChkInv.Checked)
+            {
+                T.Text = "" + Math.Exp(x);
+                ChkInv.Checked = false;
+            }
+            else
+                T.Text = "" + Math.Log(x);
+        }
+
+        private void CmdLog_Click(object sender, EventArgs e)
+        {
+            if (ChkInv.Checked)
+            {
+                T.Text = "" + Math.Pow(10.0, x);
+                ChkInv.Checked = false;
+            }
+            else
+                T.Text = "" + Math.Log(10.0, x);
+        }
+
+
+        private void CmdPi_Click(object sender, EventArgs e)
+        {
+            T.Text = "" + Math.PI;
+        }
+
+
+        private void CmdE_Click(object sender, EventArgs e)
+        {
+            T.Text = "" + Math.E;
+        }
+
+
+        private void CmdCeiling_Click(object sender, EventArgs e)
+        {
+            T.Text = "" + Math.Ceiling(x);
+        }
+
+
+        private void CmdFloor_Click(object sender, EventArgs e)
+        {
+            T.Text = "" + Math.Floor(x);
+        }
+
+
+        private void CmdRound_Click(object sender, EventArgs e)
+        {
+            T.Text = "" + Math.Floor(x);
+        }
+
+
+        private void CmdTruncate_Click(object sender, EventArgs e)
+        {
+            T.Text = "" + Math.Truncate(x);
+        }
+
+
+        private void CmdWurzel_Click(object sender, EventArgs e)
+        {
+            if (ChkInv.Checked)
+            {
+                T.Text = "" + Math.Pow(x, 2.0);
+                ChkInv.Checked = false;
+            }
+            else
+                T.Text = "" + Math.Sqrt(x);
+        }
+
+
+        private void CmdPlusMinus_Click(object sender, EventArgs e)
+        {
+            T.Text = "" + x * -1.0;
+        }
+
+
+        private void CmdKehrwert_Click(object sender, EventArgs e)
+        {
+            T.Text = "" + 1 / x;
+        }
+
+
+        private void CmdZiffer_Click(object sender, EventArgs e)
+        {
+            Button b = sender as Button; //"sender" ist ein Objekt vom Typ "object" und muss in einen "button" umgewandelt werden;
+            T.Text += b.Text;
+        }
+
+
+        private void CmdKomma_Click(object sender, EventArgs e)
+        {
+            if (T.Text.IndexOf(",") < 0) //ein Komma wird nur eingefügt wenn es noch keins gibt;
+                T.Text += ",";
+        }
+    }
+}
