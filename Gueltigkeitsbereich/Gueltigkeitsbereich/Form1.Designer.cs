1d39e5c56c8c87e051d1a09344e07ca545fad857
R-U-D-I <88041763+R-U-D-I@users.noreply.github.com>
2021-07-28 11:30:31 +0200

    Add files via upload

---
+++ Gueltigkeitsbereich/Gueltigkeitsbereich/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Gueltigkeitsbereich
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CmdAnzeige1 = new System.Windows.Forms.Button();
+            this.CmdAnzeige2 = new System.Windows.Forms.Button();
+            this.LblAnzeige = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CmdAnzeige1
+            // 
+            this.CmdAnzeige1.Location = new System.Drawing.Point(638, 66);
+            this.CmdAnzeige1.Name = "CmdAnzeige1";
+            this.CmdAnzeige1.Size = new System.Drawing.Size(150, 50);
+            this.CmdAnzeige1.TabIndex = 0;
+            this.CmdAnzeige1.Text = "Anzeige 1";
+            this.CmdAnzeige1.UseVisualStyleBackColor = true;
+            this.CmdAnzeige1.Click += new System.EventHandler(this.CmdAnzeige1_Click);
+            // 
+            // CmdAnzeige2
+            // 
+            this.CmdAnzeige2.Location = new System.Drawing.Point(638, 19);
+            this.CmdAnzeige2.Name = "CmdAnzeige2";
+            this.CmdAnzeige2.Size = new System.Drawing.Size(150, 50);
+            this.CmdAnzeige2.TabIndex = 1;
+            this.CmdAnzeige2.Text = "Anzeige 2";
+            this.CmdAnzeige2.UseVisualStyleBackColor = true;
+            this.CmdAnzeige2.Click += new System.EventHandler(this.CmdAnzeige2_Click);
+            // 
+            // LblAnzeige
+            // 
+            this.LblAnzeige.AutoSize = true;
+            this.LblAnzeige.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.LblAnzeige.Location = new System.Drawing.Point(54, 89);
+            this.LblAnzeige.Name = "LblAnzeige";
+            this.LblAnzeige.Size = new System.Drawing.Size(2, 27);
+            this.LblAnzeige.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LblAnzeige);
+            this.Controls.Add(this.CmdAnzeige2);
+            this.Controls.Add(this.CmdAnzeige1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CmdAnzeige1;
+        private System.Windows.Forms.Button CmdAnzeige2;
+        private System.Windows.Forms.Label LblAnzeige;
+    }
+}
+
---
+++ Gueltigkeitsbereich/Gueltigkeitsbereich/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gueltigkeitsbereich
+{
+    public partial class Form1 : Form
+    {
+        float x = 0.0f;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CmdAnzeige2_Click(object sender, EventArgs e)
+        {
+            float z = 0.0f;
+
+            x += 0.1f;
+            z += 0.1f;
+            LblAnzeige.Text = "X: " + x + "\n" + "Z: " + z;
+        }
+
+        private void CmdAnzeige1_Click(object sender, EventArgs e)
+        {
+            float y = 0.0f; ;
+
+            x += 0.1f;
+            y += 0.1f;
+            LblAnzeige.Text = "X: " + x + "\n" + "Y: " + y;
+        }
+    }
+}
