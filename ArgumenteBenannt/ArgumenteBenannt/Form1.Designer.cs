c6947c1e40840fc25b8a679284cc8afa751669de
R-U-D-I <88041763+R-U-D-I@users.noreply.github.com>
2021-07-28 11:15:50 +0200

    Add files via upload

---
+++ ArgumenteBenannt/ArgumenteBenannt/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace ArgumenteBenannt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CmdAnzeigen1 = new System.Windows.Forms.Button();
+            this.CmdAnzeigen2 = new System.Windows.Forms.Button();
+            this.CmdAnzeigen3 = new System.Windows.Forms.Button();
+            this.CmdAnzeigen4 = new System.Windows.Forms.Button();
+            this.LblAnzeige = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CmdAnzeigen1
+            // 
+            this.CmdAnzeigen1.Location = new System.Drawing.Point(82, 12);
+            this.CmdAnzeigen1.Name = "CmdAnzeigen1";
+            this.CmdAnzeigen1.Size = new System.Drawing.Size(75, 23);
+            this.CmdAnzeigen1.TabIndex = 0;
+            this.CmdAnzeigen1.Text = "Anzeige 1";
+            this.CmdAnzeigen1.UseVisualStyleBackColor = true;
+            this.CmdAnzeigen1.Click += new System.EventHandler(this.CmdAnzeigen1_Click);
+            // 
+            // CmdAnzeigen2
+            // 
+            this.CmdAnzeigen2.Location = new System.Drawing.Point(163, 12);
+            this.CmdAnzeigen2.Name = "CmdAnzeigen2";
+            this.CmdAnzeigen2.Size = new System.Drawing.Size(75, 23);
+            this.CmdAnzeigen2.TabIndex = 1;
+            this.CmdAnzeigen2.Text = "Anzeige 2";
+            this.CmdAnzeigen2.UseVisualStyleBackColor = true;
+            this.CmdAnzeigen2.Click += new System.EventHandler(this.CmdAnzeigen2_Click);
+            // 
+            // CmdAnzeigen3
+            // 
+            this.CmdAnzeigen3.Location = new System.Drawing.Point(244, 12);
+            this.CmdAnzeigen3.Name = "CmdAnzeigen3";
+            this.CmdAnzeigen3.Size = new System.Drawing.Size(75, 23);
+            this.CmdAnzeigen3.TabIndex = 2;
+            this.CmdAnzeigen3.Text = "Anzeige 3";
+            this.CmdAnzeigen3.UseVisualStyleBackColor = true;
+            this.CmdAnzeigen3.Click += new System.EventHandler(this.CmdAnzeigen3_Click);
+            // 
+            // CmdAnzeigen4
+            // 
+            this.CmdAnzeigen4.Location = new System.Drawing.Point(325, 12);
+            this.CmdAnzeigen4.Name = "CmdAnzeigen4";
+            this.CmdAnzeigen4.Size = new System.Drawing.Size(75, 23);
+            this.CmdAnzeigen4.TabIndex = 3;
+            this.CmdAnzeigen4.Text = "Anzeige 4";
+            this.CmdAnzeigen4.UseVisualStyleBackColor = true;
+            this.CmdAnzeigen4.Click += new System.EventHandler(this.CmdAnzeigen4_Click);
+            // 
+            // LblAnzeige
+            // 
+            this.LblAnzeige.AutoSize = true;
+            this.LblAnzeige.Location = new System.Drawing.Point(77, 75);
+            this.LblAnzeige.Name = "LblAnzeige";
+            this.LblAnzeige.Size = new System.Drawing.Size(70, 25);
+            this.LblAnzeige.TabIndex = 4;
+            this.LblAnzeige.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(600, 247);
+            this.Controls.Add(this.LblAnzeige);
+            this.Controls.Add(this.CmdAnzeigen4);
+            this.Controls.Add(this.CmdAnzeigen3);
+            this.Controls.Add(this.CmdAnzeigen2);
+            this.Controls.Add(this.CmdAnzeigen1);
+            this.Name = "Form1";
+            this.Text = "Argumente benannt";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CmdAnzeigen1;
+        private System.Windows.Forms.Button CmdAnzeigen2;
+        private System.Windows.Forms.Button CmdAnzeigen3;
+        private System.Windows.Forms.Button CmdAnzeigen4;
+        private System.Windows.Forms.Label LblAnzeige;
+    }
+}
+
---
+++ ArgumenteBenannt/ArgumenteBenannt/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArgumenteBenannt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            LblAnzeige.Text = "";
+        }
+        
+        void Rechteck(string farbe, int laenge=1, int breite=1, string rand="Linie") //die 3 letzten Parameter sind optional und werden mit Standardwerten initialisiert;
+        {
+            LblAnzeige.Text = "Farbe: " + farbe + ", Länge: " + laenge + ", Breite: " + breite + ", Rand: " + rand;
+        }
+
+        private void CmdAnzeigen1_Click(object sender, EventArgs e)
+        {
+            Rechteck("rot", 4, 6, "Punkte");
+        }
+
+        private void CmdAnzeigen2_Click(object sender, EventArgs e)
+        {
+            Rechteck("rot", rand:"Striche", breite:2, laenge:5);
+        }
+
+        private void CmdAnzeigen3_Click(object sender, EventArgs e)
+        {
+            Rechteck("gelb", 7);
+        }
+
+        private void CmdAnzeigen4_Click(object sender, EventArgs e)
+        {
+            Rechteck("blau", rand:"Haarlinie");
+        }
+    }
+}
