0574d7f525d7b9a2459305942bc2e87ff08f320e
R-U-D-I <88041763+R-U-D-I@users.noreply.github.com>
2021-07-28 12:16:27 +0200

    Add files via upload

---
+++ SwitchCase/SwitchCase/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace SwitchCase
+{
+    partial class SwitchCase
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CmdAnzeige = new System.Windows.Forms.Button();
+            this.LblAnweisung1 = new System.Windows.Forms.Label();
+            this.LblAnzeige1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CmdAnzeige
+            // 
+            this.CmdAnzeige.Location = new System.Drawing.Point(143, 47);
+            this.CmdAnzeige.Name = "CmdAnzeige";
+            this.CmdAnzeige.Size = new System.Drawing.Size(80, 40);
+            this.CmdAnzeige.TabIndex = 0;
+            this.CmdAnzeige.Text = "Enter";
+            this.CmdAnzeige.UseVisualStyleBackColor = true;
+            this.CmdAnzeige.Click += new System.EventHandler(this.CmdAnzeige_Click);
+            // 
+            // LblAnweisung1
+            // 
+            this.LblAnweisung1.AutoSize = true;
+            this.LblAnweisung1.Location = new System.Drawing.Point(12, 9);
+            this.LblAnweisung1.Name = "LblAnweisung1";
+            this.LblAnweisung1.Size = new System.Drawing.Size(129, 25);
+            this.LblAnweisung1.TabIndex = 1;
+            this.LblAnweisung1.Text = "[Anweisung]";
+            // 
+            // LblAnzeige1
+            // 
+            this.LblAnzeige1.AutoSize = true;
+            this.LblAnzeige1.Location = new System.Drawing.Point(12, 105);
+            this.LblAnzeige1.Name = "LblAnzeige1";
+            this.LblAnzeige1.Size = new System.Drawing.Size(102, 25);
+            this.LblAnzeige1.TabIndex = 2;
+            this.LblAnzeige1.Text = "[Anzeige]";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(229, 52);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 31);
+            this.textBox1.TabIndex = 3;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(17, 52);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 31);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // SwitchCase
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(406, 248);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.LblAnzeige1);
+            this.Controls.Add(this.LblAnweisung1);
+            this.Controls.Add(this.CmdAnzeige);
+            this.Name = "SwitchCase";
+            this.Text = "SwitchCase";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CmdAnzeige;
+        private System.Windows.Forms.Label LblAnweisung1;
+        private System.Windows.Forms.Label LblAnzeige1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+    }
+}
+
---
+++ SwitchCase/SwitchCase/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SwitchCase
+{
+    public partial class SwitchCase : Form
+    {
+        public SwitchCase()
+        {
+            InitializeComponent();
+
+            LblAnweisung1.Text = "Geben Sie eine Zahl ein...";
+        }
+
+        private void CmdAnzeige_Click(object sender, EventArgs e)
+        {
+            int x = (int)numericUpDown1.Value;
+            if (x == 0)
+            { 
+                    LblAnzeige1.Text = "Bitte eine andere Zahl als 0 eingeben...";
+            }
+            else
+            {
+                int y = x % 2;
+                if (y == 0)
+                {
+                    y = 2;
+                }
+                else
+                {
+                    y = 1;
+                }
+
+                switch (y)
+                {
+                    case 1:
+                        LblAnzeige1.Text = "Die eingegebene Zahl ist ungerade.";
+                        break;
+                    case 2:
+                        LblAnzeige1.Text = "Die eingegebene Zahl ist gerade.";
+                        break; //anstelle von "break" kann hier auch die Anweisung "goto case ... :" verwendet werden;
+                }
+            }
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
