17d37ab19c0e4022c1f4a7fae3a059b5d5953881
R-U-D-I <88041763+R-U-D-I@users.noreply.github.com>
2021-07-28 11:10:22 +0200

    Add files via upload

---
+++ DatenfeldVerzweigt/DatenfeldVerzweigt/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace DatenfeldVerzweigt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CmdAnzeige = new System.Windows.Forms.Button();
+            this.LblAnzeige = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CmdAnzeige
+            // 
+            this.CmdAnzeige.Location = new System.Drawing.Point(172, 12);
+            this.CmdAnzeige.Name = "CmdAnzeige";
+            this.CmdAnzeige.Size = new System.Drawing.Size(75, 23);
+            this.CmdAnzeige.TabIndex = 0;
+            this.CmdAnzeige.Text = "Anzeigen";
+            this.CmdAnzeige.UseVisualStyleBackColor = true;
+            this.CmdAnzeige.Click += new System.EventHandler(this.CmdAnzeige_Click);
+            // 
+            // LblAnzeige
+            // 
+            this.LblAnzeige.AutoSize = true;
+            this.LblAnzeige.Location = new System.Drawing.Point(12, 12);
+            this.LblAnzeige.Name = "LblAnzeige";
+            this.LblAnzeige.Size = new System.Drawing.Size(70, 25);
+            this.LblAnzeige.TabIndex = 1;
+            this.LblAnzeige.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LblAnzeige);
+            this.Controls.Add(this.CmdAnzeige);
+            this.Name = "Form1";
+            this.Text = "Datenfeld verzweigt";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CmdAnzeige;
+        private System.Windows.Forms.Label LblAnzeige;
+    }
+}
+
---
+++ DatenfeldVerzweigt/DatenfeldVerzweigt/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DatenfeldVerzweigt
+{
+    public partial class Form1 : Form
+    {
+
+        Random r = new Random();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            LblAnzeige.Text = "";
+        }
+
+
+        private void CmdAnzeige_Click(object sender, EventArgs e)
+        {
+            double[][] a = new double[5][]; //dem Hauptfeld mit Unterfeldern werden 5 Unterfelder im ersten Hauptfeld zugewiesen
+            int anz = 0;
+            //es folgt die Zuweisung der Elemente im Unterfeld
+            a[0] = new double[2];
+            a[1] = new double[4];
+            a[2] = new double[2];
+            a[3] = new double[3];
+            a[4] = new double[1];
+
+            for (int i = 0; i < a.Length; i++) //die "Length" der Elemente des Hauptfeldes a;
+            {
+                for (int k = 0; k < a[i].Length; k++) // die "Length" der Elemente der Unterfelder des Hauptfeldes a;
+                {
+                    a[i][k] = Math.Round(r.NextDouble(), 3);
+                    LblAnzeige.Text += a[i][k] + " ";
+                }
+                anz += a[i].Length;
+                LblAnzeige.Text += "\n";
+            }
+            LblAnzeige.Text += "Anzahl: " + anz;
+        }
+    }
+}
