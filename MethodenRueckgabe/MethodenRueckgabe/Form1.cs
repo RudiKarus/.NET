e546a1a5832123476e15e0e31741869c9a4b4c67
R-U-D-I <88041763+R-U-D-I@users.noreply.github.com>
2021-07-28 11:54:38 +0200

    Add files via upload

---
+++ MethodenRueckgabe/MethodenRueckgabe/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MethodenRueckgabe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            LblAnzeige.Text = "";
+        }
+
+        private void CmdAnzeigen_Click(object sender, EventArgs e)
+        {
+            int a, b, c;
+            a = 12;
+            b = 17;
+            c = MaxWert(a, b);
+            LblAnzeige.Text = "Maximun: " + c;
+        }
+
+        private int MaxWert(int x, int y)
+        {
+            if (x > y)
+                return x;
+            else
+                return y;
+        }
+    }
+}
