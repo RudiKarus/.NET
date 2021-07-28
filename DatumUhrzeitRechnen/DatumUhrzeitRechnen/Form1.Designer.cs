e54b913426cb43aaeacf1d381f47a83153764078
R-U-D-I <88041763+R-U-D-I@users.noreply.github.com>
2021-07-28 11:12:30 +0200

    Add files via upload

---
+++ DatumUhrzeitRechnen/DatumUhrzeitRechnen/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace DatumUhrzeitRechnen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LstAnzeige = new System.Windows.Forms.ListBox();
+            this.CmdAnzeigen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LstAnzeige
+            // 
+            this.LstAnzeige.FormattingEnabled = true;
+            this.LstAnzeige.ItemHeight = 25;
+            this.LstAnzeige.Location = new System.Drawing.Point(41, 74);
+            this.LstAnzeige.Name = "LstAnzeige";
+            this.LstAnzeige.Size = new System.Drawing.Size(306, 179);
+            this.LstAnzeige.TabIndex = 0;
+            // 
+            // CmdAnzeigen
+            // 
+            this.CmdAnzeigen.Location = new System.Drawing.Point(41, 45);
+            this.CmdAnzeigen.Name = "CmdAnzeigen";
+            this.CmdAnzeigen.Size = new System.Drawing.Size(75, 23);
+            this.CmdAnzeigen.TabIndex = 1;
+            this.CmdAnzeigen.Text = "Anzeigen";
+            this.CmdAnzeigen.UseVisualStyleBackColor = true;
+            this.CmdAnzeigen.Click += new System.EventHandler(this.CmdAnzeigen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CmdAnzeigen);
+            this.Controls.Add(this.LstAnzeige);
+            this.Name = "Form1";
+            this.Text = "Datum-Uhrzeit | Rechnen";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox LstAnzeige;
+        private System.Windows.Forms.Button CmdAnzeigen;
+    }
+}
+
---
+++ DatumUhrzeitRechnen/DatumUhrzeitRechnen/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DatumUhrzeitRechnen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CmdAnzeigen_Click(object sender, EventArgs e)
+        {
+            DateTime d = new DateTime(2017, 7, 18, 16, 35, 12);
+            TimeSpan ts1 = new TimeSpan(2, 10, 5);
+            TimeSpan ts2 = new TimeSpan(3, 4, 70, 10);
+
+            LstAnzeige.Items.Add("Start: " + d);
+            d = d.AddHours(3);
+            LstAnzeige.Items.Add("+3: " + d);
+            d = d.AddHours(-2.5);
+            LstAnzeige.Items.Add("-2,5 Std: " + d);
+            d = d.AddHours(34);
+            LstAnzeige.Items.Add("+34 Std: " + d);
+            d = d.AddSeconds(90);
+            LstAnzeige.Items.Add("+90 sek:" + d);
+            d = d.Add(ts1);
+            LstAnzeige.Items.Add("+2Std 10Min 5Sek:" + d);
+            d = d.Subtract(ts2);
+            LstAnzeige.Items.Add("-3Tage 4Std 70Min 10Sek: " + d);
+                
+        }
+    }
+}
