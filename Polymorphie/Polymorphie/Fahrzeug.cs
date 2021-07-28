329619cab311a7d6514dc6056a016c1b802ccd2a
R-U-D-I <88041763+R-U-D-I@users.noreply.github.com>
2021-07-28 12:01:47 +0200

    Add files via upload

---
+++ Polymorphie/Polymorphie/Fahrzeug.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphie
+{
+    class Fahrzeug
+    {
+        private string bezeichnung;
+        private int geschwindigkeit;
+
+        public Fahrzeug()
+        {
+            bezeichnung = "(leer)";
+            geschwindigkeit = 0;
+        }
+
+        public Fahrzeug(string b, int g)
+        {
+            bezeichnung = b;
+            geschwindigkeit = g;
+        }
+
+        public override string ToString()
+        {
+            return "Typ: " + GetType() + "\nBezeichnung: " + bezeichnung + "\n" + "Geschwindigkeit: " + geschwindigkeit + "\n\n";
+        }
+    }
+}
