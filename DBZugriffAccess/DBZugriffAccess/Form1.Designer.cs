83a751940818bfb310cc5abe4044f9764a18f8a5
R-U-D-I <88041763+R-U-D-I@users.noreply.github.com>
2021-07-28 11:17:17 +0200

    Add files via upload

---
+++ DBZugriffAccess/DBZugriffAccess/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace DBZugriffAccess
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CmdAlleSehen = new System.Windows.Forms.Button();
+            this.LstAnzeige = new System.Windows.Forms.ListBox();
+            this.CmdSenken = new System.Windows.Forms.Button();
+            this.CmdErhoehen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CmdAlleSehen
+            // 
+            this.CmdAlleSehen.Location = new System.Drawing.Point(40, 48);
+            this.CmdAlleSehen.Name = "CmdAlleSehen";
+            this.CmdAlleSehen.Size = new System.Drawing.Size(75, 23);
+            this.CmdAlleSehen.TabIndex = 0;
+            this.CmdAlleSehen.Text = "Alle Datensätze sehen";
+            this.CmdAlleSehen.UseVisualStyleBackColor = true;
+            this.CmdAlleSehen.Click += new System.EventHandler(this.CmdAlleSehen_Click);
+            // 
+            // LstAnzeige
+            // 
+            this.LstAnzeige.FormattingEnabled = true;
+            this.LstAnzeige.ItemHeight = 25;
+            this.LstAnzeige.Location = new System.Drawing.Point(40, 252);
+            this.LstAnzeige.Name = "LstAnzeige";
+            this.LstAnzeige.Size = new System.Drawing.Size(367, 79);
+            this.LstAnzeige.TabIndex = 1;
+            // 
+            // CmdSenken
+            // 
+            this.CmdSenken.Location = new System.Drawing.Point(40, 136);
+            this.CmdSenken.Name = "CmdSenken";
+            this.CmdSenken.Size = new System.Drawing.Size(75, 23);
+            this.CmdSenken.TabIndex = 2;
+            this.CmdSenken.Text = "Senken";
+            this.CmdSenken.UseVisualStyleBackColor = true;
+            this.CmdSenken.Click += new System.EventHandler(this.CmdAendern_Click);
+            // 
+            // CmdErhoehen
+            // 
+            this.CmdErhoehen.Location = new System.Drawing.Point(40, 107);
+            this.CmdErhoehen.Name = "CmdErhoehen";
+            this.CmdErhoehen.Size = new System.Drawing.Size(75, 23);
+            this.CmdErhoehen.TabIndex = 3;
+            this.CmdErhoehen.Text = "Erhoehen";
+            this.CmdErhoehen.UseVisualStyleBackColor = true;
+            this.CmdErhoehen.Click += new System.EventHandler(this.CmdAendern_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(468, 450);
+            this.Controls.Add(this.CmdErhoehen);
+            this.Controls.Add(this.CmdSenken);
+            this.Controls.Add(this.LstAnzeige);
+            this.Controls.Add(this.CmdAlleSehen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CmdAlleSehen;
+        private System.Windows.Forms.ListBox LstAnzeige;
+        private System.Windows.Forms.Button CmdSenken;
+        private System.Windows.Forms.Button CmdErhoehen;
+    }
+}
+
---
+++ DBZugriffAccess/DBZugriffAccess/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;//
+
+namespace DBZugriffAccess
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CmdAlleSehen_Click(object sender, EventArgs e)
+        {
+            OleDbConnection con = new OleDbConnection();
+            OleDbCommand cmd = new OleDbCommand();
+            OleDbDataReader reader;
+
+            con.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;" + @"DataSource = C:\Users\info\source\repos\Datenbank\firma1.accdb"; 
+
+            cmd.Connection = con; //es wir festgelegt auf welcher Verbindung der SQL Befehl gesendet wird;
+            cmd.CommandText = "SELECT * FROM personen"; //* bedeutet "Alle Felder" => select alle felder von tabelle;
+
+            try //da es bei einem Zugriff auf eine Datenbank zahlreiche Fehlerquellen gibt, sollte er in einem try-catch block ablaufen;
+            {
+                con.Open();
+                reader = cmd.ExecuteReader(); //ExecuteReader() sendet das Kommando; Es kommt ein Abfrageergebnis von der Klasse OleDbReader zurück und wird über den Verweis "reader";
+                LstAnzeige.Items.Clear();
+                while(reader.Read())
+                {
+                    LstAnzeige.Items.Add(reader["name"] + " # "
+                        + reader["vorname"] + " # "
+                        + reader["personalnummer"] + " # "
+                        + reader["gehalt"] + " # " + reader["geburtstag"]);
+                }
+                reader.Close();
+                con.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void CmdAendern_Click(object sender, EventArgs e)
+        {
+            OleDbConnection con = new OleDbConnection();
+            OleDbCommand cmd = new OleDbCommand();
+            int anzahl;
+            string op;
+
+            con.ConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;" + @"Data Source = C:\Users\info\source\repos\Datenbank\firma1.accdb";
+
+            cmd.Connection = con;
+
+            if (sender == CmdErhoehen)
+                op = "*";
+            else
+                op = "/";
+            cmd.CommandText = "UPDATE personen SET gehalt = gehalt" + op + "1.05";
+
+            try
+            {
+                con.Open();
+
+                anzahl = cmd.ExecuteNonQuery(); //anzahl der betroffenen Zeilen(Datensätze) wird zurückgeliefert;
+                MessageBox.Show("Datensätze geändert: " + anzahl);
+
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
