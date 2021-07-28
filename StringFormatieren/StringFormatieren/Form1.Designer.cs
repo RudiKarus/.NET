4a34d81a15ec87d6e1058fad8a10f79f18be068d
R-U-D-I <88041763+R-U-D-I@users.noreply.github.com>
2021-07-28 12:13:41 +0200

    Add files via upload

---
+++ StringFormatieren/StringFormatieren/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace StringFormatieren
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LblAnzeige = new System.Windows.Forms.Label();
+            this.LstAnzeige = new System.Windows.Forms.ListBox();
+            this.CmdAnzeigen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // LblAnzeige
+            // 
+            this.LblAnzeige.AutoSize = true;
+            this.LblAnzeige.Location = new System.Drawing.Point(7, 198);
+            this.LblAnzeige.Name = "LblAnzeige";
+            this.LblAnzeige.Size = new System.Drawing.Size(70, 25);
+            this.LblAnzeige.TabIndex = 0;
+            this.LblAnzeige.Text = "label1";
+            // 
+            // LstAnzeige
+            // 
+            this.LstAnzeige.FormattingEnabled = true;
+            this.LstAnzeige.ItemHeight = 25;
+            this.LstAnzeige.Location = new System.Drawing.Point(12, 41);
+            this.LstAnzeige.Name = "LstAnzeige";
+            this.LstAnzeige.Size = new System.Drawing.Size(361, 129);
+            this.LstAnzeige.TabIndex = 1;
+            // 
+            // CmdAnzeigen
+            // 
+            this.CmdAnzeigen.Location = new System.Drawing.Point(12, 12);
+            this.CmdAnzeigen.Name = "CmdAnzeigen";
+            this.CmdAnzeigen.Size = new System.Drawing.Size(71, 23);
+            this.CmdAnzeigen.TabIndex = 2;
+            this.CmdAnzeigen.Text = "Anzeigen";
+            this.CmdAnzeigen.UseVisualStyleBackColor = true;
+            this.CmdAnzeigen.Click += new System.EventHandler(this.CmdAnzeigen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(478, 450);
+            this.Controls.Add(this.CmdAnzeigen);
+            this.Controls.Add(this.LstAnzeige);
+            this.Controls.Add(this.LblAnzeige);
+            this.Name = "Form1";
+            this.Text = "Strings | Formatieren";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label LblAnzeige;
+        private System.Windows.Forms.ListBox LstAnzeige;
+        private System.Windows.Forms.Button CmdAnzeigen;
+    }
+}
+
---
+++ StringFormatieren/StringFormatieren/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StringFormatieren
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            LblAnzeige.Text = "";
+        }
+
+        private void CmdAnzeigen_Click(object sender, EventArgs e)
+        {
+            string format;
+            string ausgabe;
+            string[] stadt = { "München", "Berlin", "Bonn", "Bremerhaven", "Ulm" };
+
+            LstAnzeige.Items.Clear();
+
+            format = "{0,-15}{1,9:0.0000}{2,12:#,##0.0}";
+
+            for(int i=0; i < stadt.Length; i++)
+            {
+                ausgabe = String.Format(format, stadt[i], i / 7.0, i * 10000 / 7);
+                LstAnzeige.Items.Add(ausgabe);
+
+                LblAnzeige.Text += ausgabe + "\n";
+            }
+        }
+    }
+}
