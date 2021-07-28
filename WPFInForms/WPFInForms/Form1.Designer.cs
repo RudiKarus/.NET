4ee694d48382c143715d101e1c922850eeb39253
R-U-D-I <88041763+R-U-D-I@users.noreply.github.com>
2021-07-28 12:26:22 +0200

    Add files via upload

---
+++ WPFInForms/WPFInForms/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WPFInForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.WFO_Button = new System.Windows.Forms.Button();
+            this.EHost1 = new System.Windows.Forms.Integration.ElementHost();
+            this.EHost2 = new System.Windows.Forms.Integration.ElementHost();
+            this.SuspendLayout();
+            // 
+            // WFO_Button
+            // 
+            this.WFO_Button.Location = new System.Drawing.Point(12, 12);
+            this.WFO_Button.Name = "WFO_Button";
+            this.WFO_Button.Size = new System.Drawing.Size(160, 30);
+            this.WFO_Button.TabIndex = 0;
+            this.WFO_Button.Text = "button1";
+            this.WFO_Button.UseVisualStyleBackColor = true;
+            this.WFO_Button.Click += new System.EventHandler(this.WFO_Button_Click);
+            // 
+            // EHost1
+            // 
+            this.EHost1.Location = new System.Drawing.Point(12, 48);
+            this.EHost1.Name = "EHost1";
+            this.EHost1.Size = new System.Drawing.Size(160, 30);
+            this.EHost1.TabIndex = 1;
+            this.EHost1.Text = "elementHost1";
+            this.EHost1.Child = null;
+            // 
+            // EHost2
+            // 
+            this.EHost2.Location = new System.Drawing.Point(12, 84);
+            this.EHost2.Name = "EHost2";
+            this.EHost2.Size = new System.Drawing.Size(200, 100);
+            this.EHost2.TabIndex = 2;
+            this.EHost2.Text = "elementHost2";
+            this.EHost2.Child = null;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(249, 199);
+            this.Controls.Add(this.EHost2);
+            this.Controls.Add(this.EHost1);
+            this.Controls.Add(this.WFO_Button);
+            this.Name = "Form1";
+            this.Text = "WPFInForms";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button WFO_Button;
+        private System.Windows.Forms.Integration.ElementHost EHost1;
+        private System.Windows.Forms.Integration.ElementHost EHost2;
+    }
+}
+
---
+++ WPFInForms/WPFInForms/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms; //
+using System.Windows; //
+using System.Windows.Controls; //
+
+namespace WPFInForms
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //WPF Button erzeugen:
+            System.Windows.Controls.Button nb = new System.Windows.Controls.Button() 
+            {
+                Content = "WPF Button"
+            };
+            //dem Button einen EventHandler und eine Ereignismethode zuordnen:
+            nb.Click += new RoutedEventHandler(nb_Click);
+            //der Button wird dem Element "EHost1" zugeordnet:
+            EHost1.Child = nb;
+
+            StackPanel sp = new StackPanel();
+            for(int i =1; i<=3; i++)
+            {
+                TextBlock tb = new TextBlock()
+                {
+                    Text = "Inhalt " + i
+                };
+                sp.Children.Add(tb);
+            }
+            Expander ep = new Expander()
+            {
+                Header = "WPF Expander",
+                Content = sp
+            };
+            //das Element Expander wird dem Element "EHost2" zugeordnet
+            EHost2.Child = ep;
+        }
+
+
+        private void nb_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.MessageBox.Show((sender as System.Windows.Controls.Button).Content + "");
+        }
+
+
+        private void WFO_Button_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.MessageBox.Show("Windows Forms Button");
+        }
+    }
+}
