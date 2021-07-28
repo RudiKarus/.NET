4469047e7cba4166871dd03bbff3d3cc3acb44ff
R-U-D-I <88041763+R-U-D-I@users.noreply.github.com>
2021-07-28 11:01:45 +0200

    Add files via upload

---
+++ DateiSchreiben/DateiSchreiben/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace DateiSchreiben
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CmdSchreiben = new System.Windows.Forms.Button();
+            this.TxtEingabe = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // CmdSchreiben
+            // 
+            this.CmdSchreiben.Location = new System.Drawing.Point(159, 71);
+            this.CmdSchreiben.Name = "CmdSchreiben";
+            this.CmdSchreiben.Size = new System.Drawing.Size(122, 37);
+            this.CmdSchreiben.TabIndex = 0;
+            this.CmdSchreiben.Text = "Schreiben";
+            this.CmdSchreiben.UseVisualStyleBackColor = true;
+            this.CmdSchreiben.Click += new System.EventHandler(this.CmdSchreiben_Click);
+            // 
+            // TxtEingabe
+            // 
+            this.TxtEingabe.Location = new System.Drawing.Point(159, 131);
+            this.TxtEingabe.Multiline = true;
+            this.TxtEingabe.Name = "TxtEingabe";
+            this.TxtEingabe.Size = new System.Drawing.Size(122, 146);
+            this.TxtEingabe.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 372);
+            this.Controls.Add(this.TxtEingabe);
+            this.Controls.Add(this.CmdSchreiben);
+            this.Name = "Form1";
+            this.Text = "Datei schreiben";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CmdSchreiben;
+        private System.Windows.Forms.TextBox TxtEingabe;
+    }
+}
+
---
+++ DateiSchreiben/DateiSchreiben/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DateiSchreiben
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CmdSchreiben_Click(object sender, EventArgs e)
+        {
+            FileStream fs = new FileStream("aus.txt", FileMode.Create);
+            StreamWriter sw = new StreamWriter(fs);
+
+            sw.WriteLine(TxtEingabe.Text);
+
+            sw.Close();
+        }
+    }
+}
