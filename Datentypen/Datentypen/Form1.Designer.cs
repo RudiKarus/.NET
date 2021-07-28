1d1847dff493650dd2a9dafa9ffd315569193e04
R-U-D-I <88041763+R-U-D-I@users.noreply.github.com>
2021-07-28 11:10:48 +0200

    Add files via upload

---
+++ Datentypen/Datentypen/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Datentypen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CmdAnzeige = new System.Windows.Forms.Button();
+            this.LblAnzeige = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CmdAnzeige
+            // 
+            this.CmdAnzeige.Location = new System.Drawing.Point(12, 12);
+            this.CmdAnzeige.Name = "CmdAnzeige";
+            this.CmdAnzeige.Size = new System.Drawing.Size(100, 30);
+            this.CmdAnzeige.TabIndex = 0;
+            this.CmdAnzeige.Text = "Anzeigen";
+            this.CmdAnzeige.UseVisualStyleBackColor = true;
+            this.CmdAnzeige.Click += new System.EventHandler(this.CmdAnzeige_Click);
+            // 
+            // LblAnzeige
+            // 
+            this.LblAnzeige.AutoSize = true;
+            this.LblAnzeige.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.LblAnzeige.Location = new System.Drawing.Point(330, 12);
+            this.LblAnzeige.Name = "LblAnzeige";
+            this.LblAnzeige.Size = new System.Drawing.Size(2, 27);
+            this.LblAnzeige.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LblAnzeige);
+            this.Controls.Add(this.CmdAnzeige);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CmdAnzeige;
+        private System.Windows.Forms.Label LblAnzeige;
+    }
+}
+
---
+++ Datentypen/Datentypen/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Datentypen
+{
+    public partial class Form1 : Form
+    {
+        string name = "Rudi Kraus";
+        byte alter = 34;
+        float groesse = 1.83f;
+        byte gewicht = 77;
+        string rang = "Grüngurt (6.Kup)";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CmdAnzeige_Click(object sender, EventArgs e)
+        {
+            LblAnzeige.Text = name + "\n"
+                + rang + "\n"
+                + alter + " Jahre " + "\n"
+                + groesse + " m " + "\n"
+                + gewicht + " Kg " + "\n";             
+        }
+    }
+}
