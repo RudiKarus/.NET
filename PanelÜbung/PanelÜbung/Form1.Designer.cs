9f12fff371d53f94d50d49b94bb4fb49493a6980
R-U-D-I <88041763+R-U-D-I@users.noreply.github.com>
2021-07-28 12:01:13 +0200

    Add files via upload

---
+++ PanelÜbung/PanelÜbung/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace PanelÜbung
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.CmdStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(340, 179);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(50, 50);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(396, 179);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(50, 50);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(340, 235);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(50, 50);
+            this.panel3.TabIndex = 1;
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(396, 235);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(50, 50);
+            this.panel4.TabIndex = 1;
+            // 
+            // CmdStart
+            // 
+            this.CmdStart.Location = new System.Drawing.Point(297, 12);
+            this.CmdStart.Name = "CmdStart";
+            this.CmdStart.Size = new System.Drawing.Size(200, 50);
+            this.CmdStart.TabIndex = 1;
+            this.CmdStart.Text = "START";
+            this.CmdStart.UseVisualStyleBackColor = true;
+            this.CmdStart.Click += new System.EventHandler(this.CmdStart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 200;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.CmdStart);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button CmdStart;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ PanelÜbung/PanelÜbung/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PanelÜbung
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel1.BackColor = Color.FromArgb(255,100,0);
+            panel2.BackColor = Color.FromArgb(100,255,0);
+            panel3.BackColor = Color.FromArgb(0,255,200);
+            panel4.BackColor = Color.FromArgb(0,100,255);
+        }
+
+        private void CmdStart_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            panel1.Location = new Point(panel1.Location.X - 5, panel1.Location.Y - 5);
+            panel2.Location = new Point(panel2.Location.X + 5, panel2.Location.Y - 5);
+            panel3.Location = new Point(panel3.Location.X - 5, panel3.Location.Y + 5);
+            panel4.Location = new Point(panel4.Location.X + 5, panel4.Location.Y + 5);
+        }
+    }
+}
