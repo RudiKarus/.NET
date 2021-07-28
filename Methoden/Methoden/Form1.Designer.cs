ac4a97a0579cf8a3a273f8730b64e7f12a6a8f77
R-U-D-I <88041763+R-U-D-I@users.noreply.github.com>
2021-07-28 11:54:13 +0200

    Add files via upload

---
+++ Methoden/Methoden/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Methoden
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CmdAnzeige1 = new System.Windows.Forms.Button();
+            this.CmdAnzeige2 = new System.Windows.Forms.Button();
+            this.LblText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CmdAnzeige1
+            // 
+            this.CmdAnzeige1.Location = new System.Drawing.Point(12, 12);
+            this.CmdAnzeige1.Name = "CmdAnzeige1";
+            this.CmdAnzeige1.Size = new System.Drawing.Size(75, 23);
+            this.CmdAnzeige1.TabIndex = 0;
+            this.CmdAnzeige1.Text = "Anzeige 1";
+            this.CmdAnzeige1.UseVisualStyleBackColor = true;
+            this.CmdAnzeige1.Click += new System.EventHandler(this.CmdAnzeige1_Click);
+            // 
+            // CmdAnzeige2
+            // 
+            this.CmdAnzeige2.Location = new System.Drawing.Point(93, 12);
+            this.CmdAnzeige2.Name = "CmdAnzeige2";
+            this.CmdAnzeige2.Size = new System.Drawing.Size(75, 23);
+            this.CmdAnzeige2.TabIndex = 1;
+            this.CmdAnzeige2.Text = "Anzeige 2";
+            this.CmdAnzeige2.UseVisualStyleBackColor = true;
+            this.CmdAnzeige2.Click += new System.EventHandler(this.CmdAnzeige2_Click);
+            // 
+            // LblText
+            // 
+            this.LblText.AutoSize = true;
+            this.LblText.Location = new System.Drawing.Point(17, 50);
+            this.LblText.Name = "LblText";
+            this.LblText.Size = new System.Drawing.Size(70, 25);
+            this.LblText.TabIndex = 2;
+            this.LblText.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LblText);
+            this.Controls.Add(this.CmdAnzeige2);
+            this.Controls.Add(this.CmdAnzeige1);
+            this.Name = "Form1";
+            this.Text = "Methoden";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CmdAnzeige1;
+        private System.Windows.Forms.Button CmdAnzeige2;
+        private System.Windows.Forms.Label LblText;
+    }
+}
+
---
+++ Methoden/Methoden/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Methoden
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CmdAnzeige1_Click(object sender, EventArgs e)
+        {
+            double a, b;
+            a = 4.5;
+            b = 7.2;
+
+            ZeigeMaximum(a, b);
+        }
+
+        private void CmdAnzeige2_Click(object sender, EventArgs e)
+        {
+            double c, d;
+            c = 23.9;
+            d = 5.6;
+
+            ZeigeMaximum(c, d);
+        }
+
+        private void ZeigeMaximum(double x, double y)//"private" bewirkt, dass diese Methode nur in dieser "Form1" gültig ist
+        {
+            if (x > y)
+                LblText.Text = "Maximum: " + x;
+            else
+                LblText.Text = "Maximum: " + y;
+        }
+    }
+}
