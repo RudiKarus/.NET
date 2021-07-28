2e7770421b4311aa9e2d3e6b715a6ff0727f67d8
R-U-D-I <88041763+R-U-D-I@users.noreply.github.com>
2021-07-28 11:18:19 +0200

    Add files via upload

---
+++ Delegates/Delegates/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Delegates
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CmdErzeugen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CmdErzeugen
+            // 
+            this.CmdErzeugen.Location = new System.Drawing.Point(12, 12);
+            this.CmdErzeugen.Name = "CmdErzeugen";
+            this.CmdErzeugen.Size = new System.Drawing.Size(75, 23);
+            this.CmdErzeugen.TabIndex = 0;
+            this.CmdErzeugen.Text = "button1";
+            this.CmdErzeugen.UseVisualStyleBackColor = true;
+            this.CmdErzeugen.Click += new System.EventHandler(this.CmdErzeugen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(371, 376);
+            this.Controls.Add(this.CmdErzeugen);
+            this.Name = "Form1";
+            this.Text = "Delegates";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CmdErzeugen;
+    }
+}
+
---
+++ Delegates/Delegates/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Delegates
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int yPos = 44;
+        private int Nr = 1;
+
+        private void CmdErzeugen_Click(object sender, EventArgs e)
+        {
+            Button neuerButton = new Button()
+            {
+                Location = new Point(12, yPos),
+                Size = new Size(75, 26),
+                Text = Nr + ""
+            };
+
+            neuerButton.Click += new EventHandler(NeuerButton_Click); // dem Ereignis "Click" wird ein neues Objekt vom Typ "EventHandler" zugeordnet
+            Controls.Add(neuerButton); //fügt den neuen button hinzu;
+            yPos = yPos + 32;
+            Nr = Nr + 1;
+        }
+
+        private void NeuerButton_Click(object sender, EventArgs e)
+        {
+            Button cmd = sender as Button; //liefert die Info darüber welcher button gelöscht wurde an "cmd.text". Der operator "as" ermöglicht die Umwandlung von einem ungenauen typ (hier "object") in einen passenden typ (hier "Button");
+            Controls.Remove(cmd);
+            MessageBox.Show("Button" + cmd.Text + " wurde gelöscht");
+        }
+    }
+}
