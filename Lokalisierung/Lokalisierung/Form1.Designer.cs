663eb6668311699ed9dd9f402197ee704a142243
R-U-D-I <88041763+R-U-D-I@users.noreply.github.com>
2021-07-28 11:44:07 +0200

    Add files via upload

---
+++ Lokalisierung/Lokalisierung/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Lokalisierung
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.CmdEins = new System.Windows.Forms.Button();
+            this.CmdZwei = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CmdEins
+            // 
+            resources.ApplyResources(this.CmdEins, "CmdEins");
+            this.CmdEins.Name = "CmdEins";
+            this.CmdEins.UseVisualStyleBackColor = true;
+            this.CmdEins.Click += new System.EventHandler(this.CmdEins_Click);
+            // 
+            // CmdZwei
+            // 
+            resources.ApplyResources(this.CmdZwei, "CmdZwei");
+            this.CmdZwei.Name = "CmdZwei";
+            this.CmdZwei.UseVisualStyleBackColor = true;
+            this.CmdZwei.Click += new System.EventHandler(this.CmdZwei_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.CmdZwei);
+            this.Controls.Add(this.CmdEins);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CmdEins;
+        private System.Windows.Forms.Button CmdZwei;
+    }
+}
+
---
+++ Lokalisierung/Lokalisierung/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;//
+using System.Threading;//
+using System.Resources;//
+
+namespace Lokalisierung
+{
+    public partial class Form1 : Form
+    {
+        private ResourceManager rm = new ResourceManager("Lokalisierung.MeineTexte", typeof(Form1).Assembly);
+
+        public Form1()
+        {
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo("de");
+            //Thread.CurrentThread.CurrentUICulture = new CultureInfo("en");
+            //Thread.CurrentThread.CurrentUICulture = new CultureInfo("fr");
+
+            InitializeComponent();
+        }
+
+
+        private void CmdEins_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(rm.GetString("meldungEins"));
+        }
+
+
+        private void CmdZwei_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(rm.GetString("meldungZwei"));
+        }
+    }
+}
