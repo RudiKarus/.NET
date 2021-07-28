7af3995d08768980481f69f820f8ad2a863e594a
R-U-D-I <88041763+R-U-D-I@users.noreply.github.com>
2021-07-28 11:16:36 +0200

    Add files via upload

---
+++ ArgumenteOptional/ArgumenteOptional/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace ArgumenteOptional
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CmdAnzeigen1 = new System.Windows.Forms.Button();
+            this.CmdAnzeigen2 = new System.Windows.Forms.Button();
+            this.CmdAnzeigen3 = new System.Windows.Forms.Button();
+            this.LblAnzeige = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CmdAnzeigen1
+            // 
+            this.CmdAnzeigen1.Location = new System.Drawing.Point(127, 40);
+            this.CmdAnzeigen1.Name = "CmdAnzeigen1";
+            this.CmdAnzeigen1.Size = new System.Drawing.Size(75, 23);
+            this.CmdAnzeigen1.TabIndex = 0;
+            this.CmdAnzeigen1.Text = "Anzeige 1";
+            this.CmdAnzeigen1.UseVisualStyleBackColor = true;
+            this.CmdAnzeigen1.Click += new System.EventHandler(this.CmdAnzeigen1_Click);
+            // 
+            // CmdAnzeigen2
+            // 
+            this.CmdAnzeigen2.Location = new System.Drawing.Point(208, 40);
+            this.CmdAnzeigen2.Name = "CmdAnzeigen2";
+            this.CmdAnzeigen2.Size = new System.Drawing.Size(75, 23);
+            this.CmdAnzeigen2.TabIndex = 1;
+            this.CmdAnzeigen2.Text = "Anzeige 2";
+            this.CmdAnzeigen2.UseVisualStyleBackColor = true;
+            this.CmdAnzeigen2.Click += new System.EventHandler(this.CmdAnzeigen2_Click);
+            // 
+            // CmdAnzeigen3
+            // 
+            this.CmdAnzeigen3.Location = new System.Drawing.Point(289, 40);
+            this.CmdAnzeigen3.Name = "CmdAnzeigen3";
+            this.CmdAnzeigen3.Size = new System.Drawing.Size(75, 23);
+            this.CmdAnzeigen3.TabIndex = 2;
+            this.CmdAnzeigen3.Text = "Anzeige 3";
+            this.CmdAnzeigen3.UseVisualStyleBackColor = true;
+            this.CmdAnzeigen3.Click += new System.EventHandler(this.CmdAnzeigen3_Click);
+            // 
+            // LblAnzeige
+            // 
+            this.LblAnzeige.AutoSize = true;
+            this.LblAnzeige.Location = new System.Drawing.Point(122, 84);
+            this.LblAnzeige.Name = "LblAnzeige";
+            this.LblAnzeige.Size = new System.Drawing.Size(70, 25);
+            this.LblAnzeige.TabIndex = 3;
+            this.LblAnzeige.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(520, 164);
+            this.Controls.Add(this.LblAnzeige);
+            this.Controls.Add(this.CmdAnzeigen3);
+            this.Controls.Add(this.CmdAnzeigen2);
+            this.Controls.Add(this.CmdAnzeigen1);
+            this.Name = "Form1";
+            this.Text = "Argumente optional";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CmdAnzeigen1;
+        private System.Windows.Forms.Button CmdAnzeigen2;
+        private System.Windows.Forms.Button CmdAnzeigen3;
+        private System.Windows.Forms.Label LblAnzeige;
+    }
+}
+
---
+++ ArgumenteOptional/ArgumenteOptional/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArgumenteOptional
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            LblAnzeige.Text = "";
+        }
+
+        private void CmdAnzeigen1_Click(object sender, EventArgs e)
+        {
+            double a = 4.5, c = 10.3;
+            int b = 7, d = 9;
+            LblAnzeige.Text = "Ergebnis: " + Addiere(a, b, c, d);
+        }
+
+        private void CmdAnzeigen2_Click(object sender, EventArgs e)
+        {
+            double a = 4.5, c = 10.3;
+            int b = 7;
+            LblAnzeige.Text = "Ergebnis: " + Addiere(a, b, c);
+        }
+
+        private void CmdAnzeigen3_Click(object sender, EventArgs e)
+        {
+            double a = 4.5;
+            int b = 7;
+            LblAnzeige.Text = "Ergebnis: " + Addiere(a, b);
+        }
+
+
+        double Addiere(double x, int y, double z=0, int q=0 ) //z und q sind optional(wenn kein Wert angegeben dann 0)
+        {
+            return x + y + z + q;
+        }
+    }
+}
