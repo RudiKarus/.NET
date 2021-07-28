e1e15eeaddb441c89cf0fd31770f7efe9441883e
R-U-D-I <88041763+R-U-D-I@users.noreply.github.com>
2021-07-28 11:55:12 +0200

    Add files via upload

---
+++ MethodeUebergabe/MethodeUebergabe/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace MethodeUebergabe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CmdKopie = new System.Windows.Forms.Button();
+            this.CmdReferenz = new System.Windows.Forms.Button();
+            this.LblAnzeige = new System.Windows.Forms.Label();
+            this.CmdEindimFeld = new System.Windows.Forms.Button();
+            this.CmdDreidimFeld = new System.Windows.Forms.Button();
+            this.CmdOut = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CmdKopie
+            // 
+            this.CmdKopie.Location = new System.Drawing.Point(12, 12);
+            this.CmdKopie.Name = "CmdKopie";
+            this.CmdKopie.Size = new System.Drawing.Size(75, 23);
+            this.CmdKopie.TabIndex = 0;
+            this.CmdKopie.Text = "Kopie";
+            this.CmdKopie.UseVisualStyleBackColor = true;
+            this.CmdKopie.Click += new System.EventHandler(this.CmdKopie_Click);
+            // 
+            // CmdReferenz
+            // 
+            this.CmdReferenz.Location = new System.Drawing.Point(93, 12);
+            this.CmdReferenz.Name = "CmdReferenz";
+            this.CmdReferenz.Size = new System.Drawing.Size(75, 23);
+            this.CmdReferenz.TabIndex = 1;
+            this.CmdReferenz.Text = "Referenz";
+            this.CmdReferenz.UseVisualStyleBackColor = true;
+            this.CmdReferenz.Click += new System.EventHandler(this.CmdReferenz_Click);
+            // 
+            // LblAnzeige
+            // 
+            this.LblAnzeige.AutoSize = true;
+            this.LblAnzeige.Location = new System.Drawing.Point(12, 38);
+            this.LblAnzeige.Name = "LblAnzeige";
+            this.LblAnzeige.Size = new System.Drawing.Size(70, 25);
+            this.LblAnzeige.TabIndex = 2;
+            this.LblAnzeige.Text = "label1";
+            // 
+            // CmdEindimFeld
+            // 
+            this.CmdEindimFeld.Location = new System.Drawing.Point(204, 12);
+            this.CmdEindimFeld.Name = "CmdEindimFeld";
+            this.CmdEindimFeld.Size = new System.Drawing.Size(75, 23);
+            this.CmdEindimFeld.TabIndex = 3;
+            this.CmdEindimFeld.Text = "Eindimensionales Feld";
+            this.CmdEindimFeld.UseVisualStyleBackColor = true;
+            this.CmdEindimFeld.Click += new System.EventHandler(this.CmdEindimFeld_Click);
+            // 
+            // CmdDreidimFeld
+            // 
+            this.CmdDreidimFeld.Location = new System.Drawing.Point(285, 12);
+            this.CmdDreidimFeld.Name = "CmdDreidimFeld";
+            this.CmdDreidimFeld.Size = new System.Drawing.Size(75, 23);
+            this.CmdDreidimFeld.TabIndex = 4;
+            this.CmdDreidimFeld.Text = "Dreidimensionales Feld";
+            this.CmdDreidimFeld.UseVisualStyleBackColor = true;
+            this.CmdDreidimFeld.Click += new System.EventHandler(this.CmdDreidimFeld_Click);
+            // 
+            // CmdOut
+            // 
+            this.CmdOut.Location = new System.Drawing.Point(378, 12);
+            this.CmdOut.Name = "CmdOut";
+            this.CmdOut.Size = new System.Drawing.Size(75, 23);
+            this.CmdOut.TabIndex = 5;
+            this.CmdOut.Text = "Out";
+            this.CmdOut.UseVisualStyleBackColor = true;
+            this.CmdOut.Click += new System.EventHandler(this.CmdOut_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(486, 211);
+            this.Controls.Add(this.CmdOut);
+            this.Controls.Add(this.CmdDreidimFeld);
+            this.Controls.Add(this.CmdEindimFeld);
+            this.Controls.Add(this.LblAnzeige);
+            this.Controls.Add(this.CmdReferenz);
+            this.Controls.Add(this.CmdKopie);
+            this.Name = "Form1";
+            this.Text = "MethodenUebergabe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CmdKopie;
+        private System.Windows.Forms.Button CmdReferenz;
+        private System.Windows.Forms.Label LblAnzeige;
+        private System.Windows.Forms.Button CmdEindimFeld;
+        private System.Windows.Forms.Button CmdDreidimFeld;
+        private System.Windows.Forms.Button CmdOut;
+    }
+}
+
---
+++ MethodeUebergabe/MethodeUebergabe/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MethodeUebergabe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            LblAnzeige.Text = "";
+        }
+
+
+        private void CmdKopie_Click(object sender, EventArgs e)
+        {
+            int x;
+            int y;
+            x = 5;
+            y = 12;
+
+            LblAnzeige.Text = "Vorher: x: " + x + ", y: " + y;
+            TauschKopie(x, y);
+            LblAnzeige.Text += "\nNachher: x: " + x + ", y:" + y;
+        }
+
+
+        private void CmdReferenz_Click(object sender, EventArgs e)
+        {
+            int x, y;
+            x = 5;
+            y = 12;
+
+            LblAnzeige.Text = "Vorher: x: " + x + ", y: " + y;
+            TauscheReferenz(ref x, ref y);
+            LblAnzeige.Text += "\nNachher: x: " + x + ", y: " + y;
+        }
+
+
+        void TauschKopie(int a, int b) //hier werden die Werte mit dem "Ringtausch" vertauscht, bleiben aber in dieser Methode "hängen";
+        {
+            int c;
+            c = a;
+            a = b;
+            b = c;
+        }
+
+
+        void TauscheReferenz(ref int a, ref int b) //hier werden die Werte ebenfalls mit dem "Ringtausch" vertauscht und hat Auswirkung auf die Originalvariable;
+        {
+            int c;
+            c = a;
+            a = b;
+            b = c;
+        }
+
+
+        private void CmdEindimFeld_Click(object sender, EventArgs e)
+        {
+            int[] p = { 6, 7, 2 };
+
+            Verdoppeln(p);
+            LblAnzeige.Text = "";
+            foreach (int z in p)
+                LblAnzeige.Text += z + ", ";
+        }
+
+
+        void Verdoppeln(int[] x)
+        {
+            for (int i = 0; i < x.Length; i++)
+                x[i] = x[i] * 2;
+        }
+
+
+        private void CmdDreidimFeld_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            double[,,] x = new double[2, 5, 3];
+
+            for (int i = 0; i <= x.GetUpperBound(0); i++)   //GetUpperBound liefert die Anzahl der Elemente aus einer (Index)Dimension. Length liefert die Anzahl der Elemente aus ALLEN Dimensionen
+                for (int j = 0; j <= x.GetUpperBound(1); j++)
+                    for (int k = 0; k <= x.GetUpperBound(2); k++)
+                        x[i, j, k] = r.NextDouble();
+
+            MittelWert(x);
+        }
+
+
+        void MittelWert(double[,,] z)
+        {
+            double summe = 0;
+            for (int i = 0; i < z.GetUpperBound(0); i++)
+                for (int j = 0; j < z.GetUpperBound(1); j++)
+                    for (int k = 0; k < z.GetUpperBound(2); k++)
+                        summe += z[i, j, k];
+            LblAnzeige.Text = "Mittelwert: " + summe / z.Length; //geteilt durch die Anzahl ALLER Elemente aus dem ...Feld z;
+        }
+
+
+        private void CmdOut_Click(object sender, EventArgs e)
+        {
+            int a, b, c;
+            a = 12;
+            b = 3;
+
+            Addieren(a, b, out c);
+            LblAnzeige.Text = "Ergebnis: " + c;
+        }
+
+
+        void Addieren(int x, int y, out int z)
+        {
+            z = x + y;
+        }
+    }
+}
