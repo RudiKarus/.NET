d9c56e1e0111c881998504d6595a476de6777e1e
R-U-D-I <88041763+R-U-D-I@users.noreply.github.com>
2021-07-28 12:27:19 +0200

    Add files via upload

---
+++ WPFLayoutKombi/WPFLayoutKombi/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFLayoutKombi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void B1_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("B1");
+        }
+
+
+        private void B2_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("B2");
+        }
+
+
+        private void Sp_Click(object sender, RoutedEventArgs e) //object sender verweist aus das Stackpanel; Die Eigenschaft "Source" des Objekts "e" verweist auf den tatsächlich auslösenden Button;
+        {
+            Button b = e.Source as Button;
+            MessageBox.Show(b.Name);
+        }
+    }
+}
