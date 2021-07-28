d811b6131cd1c65903bfd7b43f2e784dfa351257
R-U-D-I <88041763+R-U-D-I@users.noreply.github.com>
2021-07-28 11:36:04 +0200

    Add files via upload

---
+++ Konstruktor/Konstruktor/Fahrzeug.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Konstruktor
+{
+    class Fahrzeug
+    {
+        private string bezeichnung;
+        private int geschwindigkeit;
+
+        public Fahrzeug(string b, int g)
+        {
+            bezeichnung = b;
+            geschwindigkeit = g;
+        }
+
+        public Fahrzeug(string b)
+        {
+            bezeichnung = b;
+            geschwindigkeit = 0;
+        }
+
+        public Fahrzeug(int g)
+        {
+            bezeichnung = "(leer)";
+            geschwindigkeit = g;
+        }
+
+        public Fahrzeug():this("(leer)", 0)
+        {
+        }
+
+        public override string ToString() //jede vordefinierte oder eigene Klasse erbt die Methode ToString() von der Basisklasse object; Dient zur Ausgabe der Daten als Zeichenkette;
+        {
+            return "Bezeichnung: " + bezeichnung + "\n" + "Geschwindigkeit: " + geschwindigkeit + "\n";
+        }
+       
+
+        public void beschleunigen(int wert)
+        {
+            geschwindigkeit += wert;
+        }
+    }
+}
---
+++ Konstruktor/Konstruktor/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Konstruktor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CmdAnzeige_Click(object sender, EventArgs e)
+        {
+            Fahrzeug vespa = new Fahrzeug();
+            Fahrzeug schwalbe = new Fahrzeug("Moped"); //das Objekt schwalbe erhält bei der erzeugung den Wert "Moped";
+            Fahrzeug yamaha = new Fahrzeug(50);
+            Fahrzeug honda = new Fahrzeug("Motorrad", 75);
+
+            LblAnzeige.Text = vespa +"\n"+ schwalbe + "\n" + yamaha + "\n" + honda;
+        }
+    }
+}
