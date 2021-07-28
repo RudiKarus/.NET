158a77686f59c92d1a815abd82a07003f87fd2c5
R-U-D-I <88041763+R-U-D-I@users.noreply.github.com>
2021-07-28 11:20:53 +0200

    Add files via upload

---
+++ DLLnutzen/DLLnutzen/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DLLerstellen;
+
+namespace DLLnutzen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            LblAnzeige.Text = "";
+        }
+
+        private void CmdAnzeigen_Click(object sender, EventArgs e)
+        {
+            Fahrzeug vespa = new Fahrzeug();
+            LblAnzeige.Text = vespa + "";
+            vespa.Beschleunigen(20);
+            LblAnzeige.Text += "\n" + vespa;
+        }
+    }
+}
