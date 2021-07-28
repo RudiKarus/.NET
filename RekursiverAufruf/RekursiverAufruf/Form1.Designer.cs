4dc3b9ab6606c6fa9c9392830d2ccf8c2490febc
R-U-D-I <88041763+R-U-D-I@users.noreply.github.com>
2021-07-28 12:07:08 +0200

    Add files via upload

---
+++ RekursiverAufruf/RekursiverAufruf/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace RekursiverAufruf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CmdSchleife = new System.Windows.Forms.Button();
+            this.CmdRekursion = new System.Windows.Forms.Button();
+            this.LblAnzeige = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // CmdSchleife
+            // 
+            this.CmdSchleife.Location = new System.Drawing.Point(335, 50);
+            this.CmdSchleife.Name = "CmdSchleife";
+            this.CmdSchleife.Size = new System.Drawing.Size(75, 23);
+            this.CmdSchleife.TabIndex = 0;
+            this.CmdSchleife.Text = "Schleife";
+            this.CmdSchleife.UseVisualStyleBackColor = true;
+            this.CmdSchleife.Click += new System.EventHandler(this.CmdSchleife_Click);
+            // 
+            // CmdRekursion
+            // 
+            this.CmdRekursion.Location = new System.Drawing.Point(335, 79);
+            this.CmdRekursion.Name = "CmdRekursion";
+            this.CmdRekursion.Size = new System.Drawing.Size(75, 23);
+            this.CmdRekursion.TabIndex = 1;
+            this.CmdRekursion.Text = "Rekursion";
+            this.CmdRekursion.UseVisualStyleBackColor = true;
+            this.CmdRekursion.Click += new System.EventHandler(this.CmdRekursion_Click);
+            // 
+            // LblAnzeige
+            // 
+            this.LblAnzeige.AutoSize = true;
+            this.LblAnzeige.Location = new System.Drawing.Point(51, 48);
+            this.LblAnzeige.Name = "LblAnzeige";
+            this.LblAnzeige.Size = new System.Drawing.Size(70, 25);
+            this.LblAnzeige.TabIndex = 2;
+            this.LblAnzeige.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(472, 422);
+            this.Controls.Add(this.LblAnzeige);
+            this.Controls.Add(this.CmdRekursion);
+            this.Controls.Add(this.CmdSchleife);
+            this.Name = "Form1";
+            this.Text = "Rekursiver Aufruf";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CmdSchleife;
+        private System.Windows.Forms.Button CmdRekursion;
+        private System.Windows.Forms.Label LblAnzeige;
+    }
+}
+
---
+++ RekursiverAufruf/RekursiverAufruf/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RekursiverAufruf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            LblAnzeige.Text = "";
+        }
+
+
+        private void CmdSchleife_Click(object sender, EventArgs e)
+        {
+            double x = 22;
+            LblAnzeige.Text = "x: " + x + "\n";
+
+            while (x > 0.1)
+            {
+                x = x / 2;
+                LblAnzeige.Text += "x: " + x + "\n";
+            }
+        }
+
+
+        private void CmdRekursion_Click(object sender, EventArgs e)
+        {
+            double x = 22;
+            LblAnzeige.Text= "x: " + x + "\n";
+            Halbieren(ref x);
+            LblAnzeige.Text += "x: " + x + "\n";
+        }
+
+
+        void Halbieren(ref double z)
+        {
+            z = z / 2;
+            if (z>0.1) //ohne diese Verzweigung würde sich die Methode "Halbieren()" endlos selbst aufrufen;
+            {
+                LblAnzeige.Text += "z: " + z + "\n";
+                Halbieren(ref z);
+            }
+        }
+    }
+}
