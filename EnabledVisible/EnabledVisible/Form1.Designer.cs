0c76384dc4b0124dfec6d292b769a4017bb4abe0
R-U-D-I <88041763+R-U-D-I@users.noreply.github.com>
2021-07-28 11:23:47 +0200

    Add files via upload

---
+++ EnabledVisible/EnabledVisible/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace EnabledVisible
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtEingabe1 = new System.Windows.Forms.TextBox();
+            this.TxtEingabe2 = new System.Windows.Forms.TextBox();
+            this.CmdRechnen1 = new System.Windows.Forms.Button();
+            this.CmdRechnen2 = new System.Windows.Forms.Button();
+            this.LblAusgabe = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TxtEingabe1
+            // 
+            this.TxtEingabe1.Location = new System.Drawing.Point(57, 56);
+            this.TxtEingabe1.Name = "TxtEingabe1";
+            this.TxtEingabe1.Size = new System.Drawing.Size(100, 31);
+            this.TxtEingabe1.TabIndex = 0;
+            this.TxtEingabe1.TextChanged += new System.EventHandler(this.TxtEingabe_TxtChanged);
+            // 
+            // TxtEingabe2
+            // 
+            this.TxtEingabe2.Location = new System.Drawing.Point(186, 56);
+            this.TxtEingabe2.Name = "TxtEingabe2";
+            this.TxtEingabe2.Size = new System.Drawing.Size(100, 31);
+            this.TxtEingabe2.TabIndex = 1;
+            this.TxtEingabe2.TextChanged += new System.EventHandler(this.TxtEingabe_TxtChanged);
+            // 
+            // CmdRechnen1
+            // 
+            this.CmdRechnen1.Location = new System.Drawing.Point(57, 93);
+            this.CmdRechnen1.Name = "CmdRechnen1";
+            this.CmdRechnen1.Size = new System.Drawing.Size(75, 23);
+            this.CmdRechnen1.TabIndex = 2;
+            this.CmdRechnen1.Text = "Rechnen 1";
+            this.CmdRechnen1.UseVisualStyleBackColor = true;
+            this.CmdRechnen1.Click += new System.EventHandler(this.CmdRechnen_Click);
+            // 
+            // CmdRechnen2
+            // 
+            this.CmdRechnen2.Location = new System.Drawing.Point(186, 93);
+            this.CmdRechnen2.Name = "CmdRechnen2";
+            this.CmdRechnen2.Size = new System.Drawing.Size(75, 23);
+            this.CmdRechnen2.TabIndex = 3;
+            this.CmdRechnen2.Text = "Rechnen 2";
+            this.CmdRechnen2.UseVisualStyleBackColor = true;
+            this.CmdRechnen2.Click += new System.EventHandler(this.CmdRechnen_Click);
+            // 
+            // LblAusgabe
+            // 
+            this.LblAusgabe.AutoSize = true;
+            this.LblAusgabe.Location = new System.Drawing.Point(62, 128);
+            this.LblAusgabe.Name = "LblAusgabe";
+            this.LblAusgabe.Size = new System.Drawing.Size(70, 25);
+            this.LblAusgabe.TabIndex = 4;
+            this.LblAusgabe.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(389, 186);
+            this.Controls.Add(this.LblAusgabe);
+            this.Controls.Add(this.CmdRechnen2);
+            this.Controls.Add(this.CmdRechnen1);
+            this.Controls.Add(this.TxtEingabe2);
+            this.Controls.Add(this.TxtEingabe1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtEingabe1;
+        private System.Windows.Forms.TextBox TxtEingabe2;
+        private System.Windows.Forms.Button CmdRechnen1;
+        private System.Windows.Forms.Button CmdRechnen2;
+        private System.Windows.Forms.Label LblAusgabe;
+    }
+}
+
---
+++ EnabledVisible/EnabledVisible/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EnabledVisible
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            LblAusgabe.Text = "";
+            CmdRechnen1.Enabled = false;
+            CmdRechnen2.Visible = false;
+        }
+
+        private void TxtEingabe_TxtChanged(object sender, EventArgs e)
+        {
+            if(TxtEingabe1.Text != "" && TxtEingabe2.Text != "")
+            {
+                CmdRechnen1.Enabled = true;
+                CmdRechnen2.Visible = true;
+            }
+            else
+            {
+                CmdRechnen1.Enabled = false;
+                CmdRechnen2.Visible = false;
+            }
+        }
+
+        private void CmdRechnen_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                LblAusgabe.Text = "Ergebnis: " + (Convert.ToInt32(TxtEingabe1.Text)) + (Convert.ToInt32(TxtEingabe2.Text));
+            }
+            catch
+            {
+                LblAusgabe.Text = "0";
+            }
+        }
+    }
+}
