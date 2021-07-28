bb8e6a7a4b79a2feef166172ebbee334d67f7d83
R-U-D-I <88041763+R-U-D-I@users.noreply.github.com>
2021-07-28 11:22:20 +0200

    Add files via upload

---
+++ EingabeAusgabe/EingabeAusgabe/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace EingabeAusgabe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CmdInput = new System.Windows.Forms.Button();
+            this.CmdLotto = new System.Windows.Forms.Button();
+            this.LblAnzeige = new System.Windows.Forms.Label();
+            this.CmdMsgBoxOnly = new System.Windows.Forms.Button();
+            this.CmdMsgBoxInformation = new System.Windows.Forms.Button();
+            this.CmdMsgBoxYesNo = new System.Windows.Forms.Button();
+            this.CmdMsgBoxYesNoCancel = new System.Windows.Forms.Button();
+            this.CmdMsgBoxRetryCancel = new System.Windows.Forms.Button();
+            this.CmdMsgAbortRetryIgnore = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // CmdInput
+            // 
+            this.CmdInput.Location = new System.Drawing.Point(51, 80);
+            this.CmdInput.Name = "CmdInput";
+            this.CmdInput.Size = new System.Drawing.Size(75, 23);
+            this.CmdInput.TabIndex = 0;
+            this.CmdInput.Text = "Input";
+            this.CmdInput.UseVisualStyleBackColor = true;
+            this.CmdInput.Click += new System.EventHandler(this.CmdInput_Click);
+            // 
+            // CmdLotto
+            // 
+            this.CmdLotto.Location = new System.Drawing.Point(132, 80);
+            this.CmdLotto.Name = "CmdLotto";
+            this.CmdLotto.Size = new System.Drawing.Size(75, 23);
+            this.CmdLotto.TabIndex = 1;
+            this.CmdLotto.Text = "Lotto";
+            this.CmdLotto.UseVisualStyleBackColor = true;
+            this.CmdLotto.Click += new System.EventHandler(this.CmdLotto_Click);
+            // 
+            // LblAnzeige
+            // 
+            this.LblAnzeige.AutoSize = true;
+            this.LblAnzeige.Location = new System.Drawing.Point(12, 20);
+            this.LblAnzeige.Name = "LblAnzeige";
+            this.LblAnzeige.Size = new System.Drawing.Size(70, 25);
+            this.LblAnzeige.TabIndex = 2;
+            this.LblAnzeige.Text = "label1";
+            // 
+            // CmdMsgBoxOnly
+            // 
+            this.CmdMsgBoxOnly.Location = new System.Drawing.Point(51, 109);
+            this.CmdMsgBoxOnly.Name = "CmdMsgBoxOnly";
+            this.CmdMsgBoxOnly.Size = new System.Drawing.Size(75, 23);
+            this.CmdMsgBoxOnly.TabIndex = 3;
+            this.CmdMsgBoxOnly.Text = "Ok";
+            this.CmdMsgBoxOnly.UseVisualStyleBackColor = true;
+            this.CmdMsgBoxOnly.Click += new System.EventHandler(this.CmdMsgBoxOnly_Click);
+            // 
+            // CmdMsgBoxInformation
+            // 
+            this.CmdMsgBoxInformation.Location = new System.Drawing.Point(132, 109);
+            this.CmdMsgBoxInformation.Name = "CmdMsgBoxInformation";
+            this.CmdMsgBoxInformation.Size = new System.Drawing.Size(75, 23);
+            this.CmdMsgBoxInformation.TabIndex = 4;
+            this.CmdMsgBoxInformation.Text = "Information";
+            this.CmdMsgBoxInformation.UseVisualStyleBackColor = true;
+            this.CmdMsgBoxInformation.Click += new System.EventHandler(this.CmdMsgBoxInformation_Click);
+            // 
+            // CmdMsgBoxYesNo
+            // 
+            this.CmdMsgBoxYesNo.Location = new System.Drawing.Point(51, 138);
+            this.CmdMsgBoxYesNo.Name = "CmdMsgBoxYesNo";
+            this.CmdMsgBoxYesNo.Size = new System.Drawing.Size(75, 23);
+            this.CmdMsgBoxYesNo.TabIndex = 5;
+            this.CmdMsgBoxYesNo.Text = "YesNo";
+            this.CmdMsgBoxYesNo.UseVisualStyleBackColor = true;
+            this.CmdMsgBoxYesNo.Click += new System.EventHandler(this.CmdMsgBoxYesNo_Click);
+            // 
+            // CmdMsgBoxYesNoCancel
+            // 
+            this.CmdMsgBoxYesNoCancel.Location = new System.Drawing.Point(132, 138);
+            this.CmdMsgBoxYesNoCancel.Name = "CmdMsgBoxYesNoCancel";
+            this.CmdMsgBoxYesNoCancel.Size = new System.Drawing.Size(75, 23);
+            this.CmdMsgBoxYesNoCancel.TabIndex = 6;
+            this.CmdMsgBoxYesNoCancel.Text = "YesNoCancel";
+            this.CmdMsgBoxYesNoCancel.UseVisualStyleBackColor = true;
+            this.CmdMsgBoxYesNoCancel.Click += new System.EventHandler(this.CmdMsgBoxYesNoCancel_Click);
+            // 
+            // CmdMsgBoxRetryCancel
+            // 
+            this.CmdMsgBoxRetryCancel.Location = new System.Drawing.Point(51, 167);
+            this.CmdMsgBoxRetryCancel.Name = "CmdMsgBoxRetryCancel";
+            this.CmdMsgBoxRetryCancel.Size = new System.Drawing.Size(75, 23);
+            this.CmdMsgBoxRetryCancel.TabIndex = 7;
+            this.CmdMsgBoxRetryCancel.Text = "Retry Cancel";
+            this.CmdMsgBoxRetryCancel.UseVisualStyleBackColor = true;
+            this.CmdMsgBoxRetryCancel.Click += new System.EventHandler(this.CmdMsgBoxRetryCancel_Click);
+            // 
+            // CmdMsgAbortRetryIgnore
+            // 
+            this.CmdMsgAbortRetryIgnore.Location = new System.Drawing.Point(132, 167);
+            this.CmdMsgAbortRetryIgnore.Name = "CmdMsgAbortRetryIgnore";
+            this.CmdMsgAbortRetryIgnore.Size = new System.Drawing.Size(75, 23);
+            this.CmdMsgAbortRetryIgnore.TabIndex = 8;
+            this.CmdMsgAbortRetryIgnore.Text = "Abort Retry Ignore";
+            this.CmdMsgAbortRetryIgnore.UseVisualStyleBackColor = true;
+            this.CmdMsgAbortRetryIgnore.Click += new System.EventHandler(this.CmdMsgAbortRetryIgnore_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(295, 228);
+            this.Controls.Add(this.CmdMsgAbortRetryIgnore);
+            this.Controls.Add(this.CmdMsgBoxRetryCancel);
+            this.Controls.Add(this.CmdMsgBoxYesNoCancel);
+            this.Controls.Add(this.CmdMsgBoxYesNo);
+            this.Controls.Add(this.CmdMsgBoxInformation);
+            this.Controls.Add(this.CmdMsgBoxOnly);
+            this.Controls.Add(this.LblAnzeige);
+            this.Controls.Add(this.CmdLotto);
+            this.Controls.Add(this.CmdInput);
+            this.Name = "Form1";
+            this.Text = "Eingabe und Ausgabe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CmdInput;
+        private System.Windows.Forms.Button CmdLotto;
+        private System.Windows.Forms.Label LblAnzeige;
+        private System.Windows.Forms.Button CmdMsgBoxOnly;
+        private System.Windows.Forms.Button CmdMsgBoxInformation;
+        private System.Windows.Forms.Button CmdMsgBoxYesNo;
+        private System.Windows.Forms.Button CmdMsgBoxYesNoCancel;
+        private System.Windows.Forms.Button CmdMsgBoxRetryCancel;
+        private System.Windows.Forms.Button CmdMsgAbortRetryIgnore;
+    }
+}
+
---
+++ EingabeAusgabe/EingabeAusgabe/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic; //Project->Add Reference->Assemblies->Framework
+
+namespace EingabeAusgabe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            LblAnzeige.Text = "";
+        }
+
+
+        private void CmdInput_Click(object sender, EventArgs e)
+        {
+            string eingabe;
+
+            eingabe = Interaction.InputBox("Bitte geben Sie Ihren Namen ein:", "Name", "Maier");
+
+            LblAnzeige.Text = eingabe;
+        }
+
+
+        private void CmdLotto_Click(object sender, EventArgs e)
+        {
+            int zahl;
+            int[] lotto = new int[6];
+            bool gezogen;
+
+            LblAnzeige.Text = "";
+
+            for(int i=0; i < lotto.Length; i++)
+            {
+                do
+                {
+                    gezogen = false;
+                    zahl = 0;
+
+                    try
+                    {
+                        zahl = Convert.ToInt32(Interaction.InputBox("Zahl " + (i + 1) + ":", "Zahl " + (i + 1)));
+                    }
+                    catch
+                    {
+                        continue;
+                    }
+
+                    for (int k=0; k < i; k++)
+                    {
+                        if(lotto[k] == zahl)
+                        {
+                            gezogen = true;
+                            break;
+                        }
+                    }
+
+                }
+                while (gezogen || zahl < 1 || zahl > 49);
+
+                lotto[i] = zahl;
+                LblAnzeige.Text += zahl + " ";
+            }
+        }
+
+
+        private void CmdMsgBoxOnly_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Gelesen? Dann bitte OK drücken.", "OK", MessageBoxButtons.OK);
+        }
+
+
+        private void CmdMsgBoxInformation_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Das ist eine Information", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+
+        private void CmdMsgBoxYesNo_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Soll die Datei gesichert werden?", "Sicherung", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (dr == DialogResult.Yes)
+                LblAnzeige.Text = "Sichern.";
+            else
+                LblAnzeige.Text = "Nicht sichern.";
+        }
+
+
+        private void CmdMsgBoxYesNoCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Soll die Datei gesichert werden?", "Sicherung", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Question);
+
+            if (dr == DialogResult.Yes)
+                LblAnzeige.Text = "Sichern";
+            else if (dr == DialogResult.No)
+                LblAnzeige.Text = "Nicht sichern";
+            else
+                LblAnzeige.Text = "Abbrechen";
+        }
+
+
+        private void CmdMsgBoxRetryCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Beim Sichern der Datei ist ein Fehler aufgetreten.\n" + "Möchten Sie es noch einmal probieren?\n" + "Wollen sie den Vorgang abbrechen?", "Fehler bei Sicherung", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+
+            if (dr == DialogResult.Retry)
+                LblAnzeige.Text = "Noch einmal";
+            else
+                LblAnzeige.Text = "Abbrechen";
+        }
+
+
+        private void CmdMsgAbortRetryIgnore_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = MessageBox.Show("Beim sichern der Datei ist ein Fehler aufgetreten", "Fehler bei Sicherung", MessageBoxButtons.AbortRetryIgnore, MessageBoxIcon.Warning);
+
+            if (dr == DialogResult.Abort)
+                LblAnzeige.Text = "Abbrechen";
+            else if (dr == DialogResult.Retry)
+                LblAnzeige.Text = "Noch einmal";
+            else
+                LblAnzeige.Text = "Ignorieren";
+        }
+    }
+}
